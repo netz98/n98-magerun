e20e87a5ac97c6354e29e4b19247eafa95ca419c
Christian Münch <c.muench@netz98.de>
2012-09-03 14:20:48 +0200

    Added data updates to "system:run-setup-scripts" command.

--- src/N98/Magento/Application.php
+++ src/N98/Magento/Application.php
@@ -56,7 +56,7 @@ class Application extends BaseApplication
     /**
      * @var string
      */
-    const APP_VERSION = '1.13.1';
+    const APP_VERSION = '1.13.2';
 
     public function __construct($autoloader)
     {
--- src/N98/Magento/Command/System/RunSetupScriptsCommand.php
+++ src/N98/Magento/Command/System/RunSetupScriptsCommand.php
@@ -23,7 +23,10 @@ class RunSetupScriptsCommand extends AbstractMagentoCommand
     {
         $this->detectMagento($output);
         if ($this->initMagento()) {
-            \Mage_Core_Model_Resource_Setup::applyAllUpdates();
+            \Mage_Core_Model_Resource_Setup::applyUpdates();
+            if (is_callable(array('\Mage_Core_Model_Resource_Setup', 'applyAllDataUpdates'))) {
+                \Mage_Core_Model_Resource_Setup::applyAllDataUpdates();
+            }
             $output->writeln('<info>done</info>');
         }
     }
