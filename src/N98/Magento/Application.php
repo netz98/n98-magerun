931118b3b280e604f29cbefa3d460c855468db92
Christian Münch <c.muench@netz98.de>
2012-09-30 21:47:26 +0200

    Reformatted dev:module:rewrite:conflict table.

--- src/N98/Magento/Application.php
+++ src/N98/Magento/Application.php
@@ -57,7 +57,7 @@ class Application extends BaseApplication
     /**
      * @var string
      */
-    const APP_VERSION = '1.24.3';
+    const APP_VERSION = '1.24.4';
 
     /**
      * @var \Composer\Autoload\ClassLoader
--- src/N98/Magento/Command/Developer/Module/Rewrite/ConflictsCommand.php
+++ src/N98/Magento/Command/Developer/Module/Rewrite/ConflictsCommand.php
@@ -26,34 +26,36 @@ class ConflictsCommand extends AbstractRewriteCommand
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $this->detectMagento($output, true);
-        $this->writeSection($output, 'Conflicts');
         if ($this->initMagento()) {
-            $rewrites = $this->loadRewrites();
-            $table = array();
-            $conflictCounter = 0;
-            foreach ($rewrites as $type => $data) {
-                if (count($data) > 0) {
-                    foreach ($data as $class => $rewriteClass) {
-                        if (count($rewriteClass) > 1) {
-                            $table[] = array(
-                                'Type'         => $type,
-                                'Class'        => $class,
-                                'Rewrites'     => implode(', ', $rewriteClass),
-                                'Loaded Class' => \Mage::getConfig()->getModelClassName($class),
-                            );
-                            $conflictCounter++;
+            $this->writeSection($output, 'Conflicts');
+            $table = new \Zend_Text_Table(array('columnWidths' => array(8, 30, 60, 60)));
+            if ($this->initMagento()) {
+                $rewrites = $this->loadRewrites();
+                $conflictCounter = 0;
+                foreach ($rewrites as $type => $data) {
+                    if (count($data) > 0) {
+                        foreach ($data as $class => $rewriteClass) {
+                            if (count($rewriteClass) > 1) {
+                                $table->appendRow(array(
+                                    'Type'         => $type,
+                                    'Class'        => $class,
+                                    'Rewrites'     => implode(', ', $rewriteClass),
+                                    'Loaded Class' => \Mage::getConfig()->getModelClassName($class),
+                                ));
+                                $conflictCounter++;
+                            }
                         }
                     }
                 }
-            }
 
-            if (count($table) > 0) {
-                $output->writeln('<error>' . $conflictCounter . ' conflict' . ($conflictCounter > 1 ? 's' : '') . ' was found!</error>');
-                $this->getHelper('table')->write($output, $table);
-            } else {
-                $output->writeln('<info>No rewrite conflicts was found.</info>');
-            }
+                if (count($table) > 0) {
+                    $output->writeln('<error>' . $conflictCounter . ' conflict' . ($conflictCounter > 1 ? 's' : '') . ' was found!</error>');
+                    $output->write($table->render());
+                } else {
+                    $output->writeln('<info>No rewrite conflicts was found.</info>');
+                }
 
+            }
         }
     }
 }
