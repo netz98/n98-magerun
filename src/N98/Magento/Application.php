e6c992defc4ead1f9f6f1ab2b31b6a71bf1e830c
Alexander Menk <alex.menk@gmail.com>
2012-10-08 20:58:06 +0200

    Changed version number to a branched one (must be changed to a main-one when merging)

--- src/N98/Magento/Application.php
+++ src/N98/Magento/Application.php
@@ -62,7 +62,7 @@ class Application extends BaseApplication
     /**
      * @var string
      */
-    const APP_VERSION = '1.28.0';
+    const APP_VERSION = '1.28.0-am1';
 
     /**
      * @var \Composer\Autoload\ClassLoader
