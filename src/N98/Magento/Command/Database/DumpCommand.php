142e2d192fbde4dad59b3e5fe1daef5422910744
Alexander Menk <a.menk@netz98.de>
2012-10-11 17:11:49 +0200

    #5 database:dump: Added draft config and help for stripped dumps (no functionality yet)

--- src/N98/Magento/Command/Database/DumpCommand.php
+++ src/N98/Magento/Command/Database/DumpCommand.php
@@ -11,14 +11,57 @@ class DumpCommand extends AbstractDatabaseCommand
 {
     protected function configure()
     {
+
         $this
             ->setName('database:dump')
             ->setAliases(array('db:dump'))
             ->addArgument('filename', InputArgument::OPTIONAL, 'Dump filename')
             ->addOption('only-command', null, InputOption::VALUE_NONE, 'Print only mysqldump command. Do not execute')
+            ->addArgument('strip', InputArgument::OPTIONAL, 'Tables to strip (dump only structure) <error>foo</error>')
             ->setDescription('Dumps database with mysqldump cli client according to informations from local.xml')
         ;
     }
+
+
+    public function getTableDefinitionHelp()
+    {
+        $messages = array();
+        $this->commandConfig = $this->getCommandConfig();
+        $messages[] = '';
+        $messages[] = '<comment>Strip parameter</comment>';
+        $messages[] = ' Separate each table to strip by a space.';
+        $messages[] = ' You can use wildcards like * and ? in the table names to strip multiple tables.';
+        $messages[] = ' In addition you can specify pre-defined table groups, that start with an @';
+        $messages[] = ' Example: "dataflow_batch_export unimportant_module_* @log';
+        $messages[] = '<comment>Available Table Groups</comment>';
+        if (isset($this->commandConfig['table-groups'])) {
+            $tableGroups = $this->commandConfig['table-groups'];
+            foreach($tableGroups as $index=>$definition) {
+                $description = isset($definition['description']) ? $definition['description'] : '';
+                if (!isset($definition['id'])) {
+                    throw new \Exception('Invalid definition of table-groups (id missing) Index: '.$index);
+                }
+                /** @TODO:
+                 * Column-Wise formating of the options, see InputDefinition::asText for code to pad by the max length,
+                 * but I do not like to copy and paste ..
+                 */
+                $messages[] = ' <info>@'.$definition['id'].'</info> ' . $description;
+            }
+        }
+        return implode("\n", $messages);
+
+    }
+    public function asText() {
+        return parent::asText() . "\n" .
+            $this->getTableDefinitionHelp();
+    }
+
+    protected function getTableGroups()
+    {
+
+
+    }
+
 
     /**
      * @param \Symfony\Component\Console\Input\InputInterface $input
