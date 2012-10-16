4c92abc95428c9f9d7c274097556b7a4c2b11eb1
Christian Münch <c.muench@netz98.de>
2012-10-16 13:05:44 +0200

    Little fix for CI server integration.

--- src/N98/Magento/Application.php
+++ src/N98/Magento/Application.php
@@ -62,7 +62,7 @@ class Application extends BaseApplication
     /**
      * @var string
      */
-    const APP_VERSION = '1.30.1';
+    const APP_VERSION = '1.30.2';
 
     /**
      * @var \Composer\Autoload\ClassLoader
