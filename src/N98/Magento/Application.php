d9dc1bc1056cac14d16e0ba5d28c61bc6e8a05b8
Christian Münch <c.muench@netz98.de>
2012-10-17 15:23:07 +0200

    Fixed broken model detection.

--- src/N98/Magento/Application.php
+++ src/N98/Magento/Application.php
@@ -62,7 +62,7 @@ class Application extends BaseApplication
     /**
      * @var string
      */
-    const APP_VERSION = '1.30.3';
+    const APP_VERSION = '1.30.4';
 
     /**
      * @var \Composer\Autoload\ClassLoader
