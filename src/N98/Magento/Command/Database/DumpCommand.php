cb2eb6f2f053e58e5e7c810b33b8c0d187ab48d6
Alexander Menk <alex.menk@gmail.com>
2012-11-11 18:17:28 +0100

    db:dump Export only structure for certain tables [beta]

--- src/N98/Magento/Command/Database/DumpCommand.php
+++ src/N98/Magento/Command/Database/DumpCommand.php
@@ -9,6 +9,9 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class DumpCommand extends AbstractDatabaseCommand
 {
+
+    protected $tableDefinitions = null;
+
     protected function configure()
     {
 
@@ -16,16 +19,50 @@ class DumpCommand extends AbstractDatabaseCommand
             ->setName('db:dump')
             ->setAliases(array('database:dump'))
             ->addArgument('filename', InputArgument::OPTIONAL, 'Dump filename')
+            ->addArgument('strip', InputArgument::OPTIONAL, 'Tables to strip (dump only structure of those tables)')
             ->addOption('add-time', null, InputOption::VALUE_NONE, 'Adds time to filename (only if filename was not provided)')
             ->addOption('only-command', null, InputOption::VALUE_NONE, 'Print only mysqldump command. Do not execute')
-            ->addArgument('strip', InputArgument::OPTIONAL, 'Tables to strip (dump only structure) <error>foo</error>')
-            ->addOption('stdout', null, InputOption::VALUE_NONE, 'Dump to stdout')
+            ->addOption('no-single-transaction', null, InputOption::VALUE_NONE, 'Do not use single-transaction (not recommended, this is blocking)')
+            ->addOption('stdout', null, InputArgument::OPTIONAL, 'Dump to stdout')
             ->addDeprecatedAlias('database:dump', 'Please use db:dump')
-            ->setDescription('Dumps database with mysqldump cli client according to informations from local.xml')
-        ;
+            ->setDescription('Dumps database with mysqldump cli client according to informations from local.xml');
     }
 
 
+    public function getTableDefinitions()
+    {
+        $this->commandConfig = $this->getCommandConfig();
+
+        if(is_null($this->tableDefinitions)) {
+            $this->tableDefinitions = array();
+            if (isset($this->commandConfig['table-groups'])) {
+                $tableGroups = $this->commandConfig['table-groups'];
+                foreach($tableGroups as $index=>$definition) {
+                    $description = isset($definition['description']) ? $definition['description'] : '';
+                    if (!isset($definition['id'])) {
+                        throw new \Exception('Invalid definition of table-groups (id missing) Index: '.$index);
+                    }
+                    if (!isset($definition['id'])) {
+                        throw new \Exception('Invalid definition of table-groups (tables missing) Id: '.$definition['id']);
+                    }
+
+                    $this->tableDefinitions[$definition['id']] = array(
+                        'tables' => $definition['tables'],
+                        'description' => $description,
+                    );
+                }
+            };
+        }
+
+        return $this->tableDefinitions;
+    }
+
+    /**
+     * Generate help for table defintions
+     *
+     * @return string
+     * @throws \Exception
+     */
     public function getTableDefinitionHelp()
     {
         $messages = array();
@@ -37,32 +74,66 @@ class DumpCommand extends AbstractDatabaseCommand
         $messages[] = ' In addition you can specify pre-defined table groups, that start with an @';
         $messages[] = ' Example: "dataflow_batch_export unimportant_module_* @log';
         $messages[] = '<comment>Available Table Groups</comment>';
-        if (isset($this->commandConfig['table-groups'])) {
-            $tableGroups = $this->commandConfig['table-groups'];
-            foreach($tableGroups as $index=>$definition) {
-                $description = isset($definition['description']) ? $definition['description'] : '';
-                if (!isset($definition['id'])) {
-                    throw new \Exception('Invalid definition of table-groups (id missing) Index: '.$index);
-                }
-                /** @TODO:
-                 * Column-Wise formating of the options, see InputDefinition::asText for code to pad by the max length,
-                 * but I do not like to copy and paste ..
-                 */
-                $messages[] = ' <info>@'.$definition['id'].'</info> ' . $description;
-            }
+
+        $definitions = $this->getTableDefinitions();
+        foreach($definitions as $id=>$definition) {
+            $description = isset($definition['description']) ? $definition['description'] : '';
+            /** @TODO:
+             * Column-Wise formating of the options, see InputDefinition::asText for code to pad by the max length,
+             * but I do not like to copy and paste ..
+             */
+            $messages[] = ' <info>@'.$id.'</info> ' . $description;
         }
         return implode("\n", $messages);
 
     }
+
     public function asText() {
         return parent::asText() . "\n" .
             $this->getTableDefinitionHelp();
     }
 
-    protected function getTableGroups()
+    /**
+     * @param $param
+     * @param array $resolved Which definitions where already resolved -> prevent endless loops
+     */
+    protected function resolveTables($excludes, $resolved = array())
     {
+        $definitions = $this->getTableDefinitions();
 
+        $resolvedExcludes = array();
+        foreach($excludes as $exclude) {
+            if (substr($exclude, 0, 1) == '@') {
+                $code = substr($exclude, 1);
+                if (!isset($definitions[$code])) {
+                    throw new \Exception('Table-groups could not be resolved: '.$exclude);
+                }
+                if (!isset($resolved[$code])) {
+                    $resolved[$code] = true;
+                    $tables = $this->resolveTables(explode(' ', $definitions[$code]['tables']), $resolved);
+                    $resolvedExcludes = array_merge($resolvedExcludes, $tables);
+                }
+                continue;
+            }
 
+            // resolve wildcards
+            if (strpos($exclude, '*')) {
+                $connection = $this->_getConnection();
+                $sth = $connection->prepare('SHOW TABLES LIKE :like', array(\PDO::ATTR_CURSOR => \PDO::CURSOR_FWDONLY));
+                $sth->execute(
+                    array(':like' => str_replace('*', '%', $exclude))
+                );
+                $rows = $sth->fetchAll();
+                foreach($rows as $row) {
+                    $resolvedExcludes[] = $row[0];
+                }
+                continue;
+            }
+
+            $resolvedExcludes[] = $exclude;
+        }
+
+        return $resolvedExcludes;
     }
 
 
@@ -91,28 +162,73 @@ class DumpCommand extends AbstractDatabaseCommand
             $fileName .= '.sql';
         }
 
-        $exec = 'mysqldump ' . $this->getMysqlClientToolConnectionString();
-        if (!$input->getOption('stdout')) {
-            $exec .= ' > ' . escapeshellarg($fileName);
+        if ($input->getArgument('strip')) {
+            $stripTables = $this->resolveTables(explode(' ', $input->getArgument('strip')));
+            if (!$input->getOption('stdout')) {
+                $output->writeln('<comment>No-data export for: <info>' . implode(' ',$stripTables) . '</info></comment>');
+            }
+        } else {
+            $stripTables = false;
+        }
+
+
+        if ($input->getOption('no-single-transaction')) {
+            $dumpOptions = '--single-transaction ';
+        } else {
+            $dumpOptions = '';
+        }
+
+        $execs = array();
+
+
+        if (!$stripTables) {
+            $exec = 'mysqldump ' . $dumpOptions . $this->getMysqlClientToolConnectionString();
+            if (!$input->getOption('stdout')) {
+                $exec .= ' > ' . escapeshellarg($fileName);
+            }
+            $execs[] = $exec;
+        } else {
+            // dump structure for strip-tables
+            $exec = 'mysqldump ' . $dumpOptions . '--no-data ' . $this->getMysqlClientToolConnectionString();
+            $exec .= ' ' . implode(' ', $stripTables);
+            if (!$input->getOption('stdout')) {
+                $exec .= ' > ' . escapeshellarg($fileName);
+            }
+            $execs[] = $exec;
+
+            $ignore = '';
+            foreach($stripTables as $stripTable) {
+                $ignore .= '--ignore-table=' . $this->dbSettings['dbname'] . '.' . $stripTable . ' ';
+            }
+            // dump data for all other tables
+            $exec = 'mysqldump ' . $dumpOptions . $ignore . $this->getMysqlClientToolConnectionString();
+            if (!$input->getOption('stdout')) {
+                $exec .= ' >> ' . escapeshellarg($fileName);
+            }
+            $execs[] = $exec;
         }
 
         if ($input->getOption('only-command')) {
-            $output->writeln($exec);
+            foreach($execs as $exec) {
+                $output->writeln($exec);
+            }
         } else {
             if (!$input->getOption('stdout')) {
                 $output->writeln('<comment>Start dumping database: <info>' . $this->dbSettings['dbname'] . '</info> to file <info>' . $fileName . '</info>');
             }
-            if ($input->getOption('stdout')) {
-                passthru($exec, $returnValue);
-            } else {
-                exec($exec, $commandOutput, $returnValue);
-            }
-            if ($returnValue > 0) {
-                $output->writeln('<error>' . implode(PHP_EOL, $commandOutput) . '</error>');
-            } else {
-                if (!$input->getOption('stdout')) {
-                    $output->writeln('<info>Finished</info>');
+
+            foreach($execs as $exec) {
+                if ($input->getOption('stdout')) {
+                    passthru($exec, $returnValue);
+                } else {
+                    exec($exec, $commandOutput, $returnValue);
                 }
+                if ($returnValue > 0) {
+                    $output->writeln('<error>' . implode(PHP_EOL, $commandOutput) . '</error>');
+                }
+            }
+            if (!$input->getOption('stdout')) {
+                $output->writeln('<info>Finished</info>');
             }
         }
     }
