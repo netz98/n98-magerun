20f80e9cba8ebb5178685cb5ee825cf87b0b6732
Alexander Menk <alex.menk@gmail.com>
2012-11-11 18:33:30 +0100

    Version 1.37.1

--- src/N98/Magento/Application.php
+++ src/N98/Magento/Application.php
@@ -69,7 +69,7 @@ class Application extends BaseApplication
     /**
      * @var string
      */
-    const APP_VERSION = '1.37.0';
+    const APP_VERSION = '1.37.1';
 
     /**
      * @var \Composer\Autoload\ClassLoader
