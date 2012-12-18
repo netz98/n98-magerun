d9a00ccc5c574b14af3d62ce583e128eb0e66955
Christian Münch <c.muench@netz98.de>
2012-12-18 10:48:51 +0100

    Fix for issue #62 -> Stripped DB:dump @sales group does not empty sales_flat_order table

--- src/N98/Magento/Application.php
+++ src/N98/Magento/Application.php
@@ -81,7 +81,7 @@ class Application extends BaseApplication
     /**
      * @var string
      */
-    const APP_VERSION = '1.40.0';
+    const APP_VERSION = '1.40.1';
 
     /**
      * @var \Composer\Autoload\ClassLoader
