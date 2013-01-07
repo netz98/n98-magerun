36ed76d5fb0a418103438a4ae23d96a2fc8728eb
Christian Münch <c.muench@netz98.de>
2013-01-08 00:38:23 +0100

    Fixed docblock.

--- src/N98/Magento/Command/Customer/InfoCommand.php
+++ src/N98/Magento/Command/Customer/InfoCommand.php
@@ -26,8 +26,8 @@ class InfoCommand extends AbstractCustomerCommand
     }
 
     /**
-     * @param \Symfony\Component\Console\Input\\Symfony\Component\Console\Input\InputInterface $input
-     * @param \Symfony\Component\Console\Output\\Symfony\Component\Console\Output\OutputInterface $output
+     * @param \Symfony\Component\Console\Input\InputInterface $input
+     * @param \Symfony\Component\Console\Output\OutputInterface $output
      * @return int|void
      */
     protected function execute(InputInterface $input, OutputInterface $output)
