7b8f5f573a654673f2b55b45485a78938b8b364c
Christian Münch <c.muench@netz98.de>
2013-02-19 16:55:08 +0100

    Fixed issue #81 Fix for cases where a module may be installed that doesn't have a config.xml

--- src/N98/Magento/Application.php
+++ src/N98/Magento/Application.php
@@ -98,7 +98,7 @@ class Application extends BaseApplication
     /**
      * @var string
      */
-    const APP_VERSION = '1.52.1';
+    const APP_VERSION = '1.52.2';
 
     /**
      * @var \Composer\Autoload\ClassLoader
