b338ddd26098831c16590ab9294cd7a192f342dd
Christian Münch <c.muench@netz98.de>
2013-03-02 10:35:00 +0100

    Updated symfony components to V2.2.0

--- src/N98/Magento/Application.php
+++ src/N98/Magento/Application.php
@@ -98,7 +98,7 @@ class Application extends BaseApplication
     /**
      * @var string
      */
-    const APP_VERSION = '1.52.2';
+    const APP_VERSION = '1.53.0';
 
     /**
      * @var \Composer\Autoload\ClassLoader
