d83de28d7e2a6a687f2dee82600931a7e24a551a
Alexander Menk <a.menk@netz98.de>
2013-03-07 10:43:31 +0100

    Version 1.54.2

--- src/N98/Magento/Application.php
+++ src/N98/Magento/Application.php
@@ -97,7 +97,7 @@ class Application extends BaseApplication
     /**
      * @var string
      */
-    const APP_VERSION = '1.54.1';
+    const APP_VERSION = '1.54.2';
 
     /**
      * @var \Composer\Autoload\ClassLoader
