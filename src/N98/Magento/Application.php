7b23c8d792890c9d37dd26995efdada310bc0fdb
Alexander Menk <a.menk@netz98.de>
2013-03-12 12:37:54 +0100

    #98: Improved db:dump --strip handling of errors and duplicate table names (1.54.3)

--- src/N98/Magento/Application.php
+++ src/N98/Magento/Application.php
@@ -97,7 +97,7 @@ class Application extends BaseApplication
     /**
      * @var string
      */
-    const APP_VERSION = '1.54.2';
+    const APP_VERSION = '1.54.3';
 
     /**
      * @var \Composer\Autoload\ClassLoader
