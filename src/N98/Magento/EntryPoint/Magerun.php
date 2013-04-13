855d7af281fbc237b7e36aebe7dd84c565e4136e
Christian Münch <c.muench@netz98.de>
2013-04-13 22:43:35 +0200

    Ignore Magento 2 Entry Point in Code Coverage.

--- src/N98/Magento/EntryPoint/Magerun.php
+++ src/N98/Magento/EntryPoint/Magerun.php
@@ -2,6 +2,13 @@
 
 namespace N98\Magento\EntryPoint;
 
+/**
+ * Class Magerun
+ * This is required for Magento 2
+ *
+ * @codeCoverageIgnore
+ * @package N98\Magento\EntryPoint
+ */
 class Magerun extends \Mage_Core_Model_EntryPointAbstract
 {
     public function _processRequest()
