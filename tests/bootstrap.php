5016478dcefcb8e6b6464361aa50097831d3d231
Christian Münch <c.muench@netz98.de>
2013-04-28 22:32:39 +0200

    [BUGFIX] Added fix for PHPUnit/Varien_Autoload problem
    Steps:
    
    - Init Magento
    - Removed Varien_Autoload over spl_autoload_unregister
    - Add Magento include pathes to Composer autoloader

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,15 @@
 <?php
 
-require_once __DIR__ . '/../vendor/autoload.php';
+$loader = require __DIR__ . '/../vendor/autoload.php';
+/* @var $loader \Composer\Autoload\ClassLoader */
+$loader->setUseIncludePath(true);
+
+$base = getenv('N98_MAGERUN_TEST_MAGENTO_ROOT');
+$paths = array(
+    $base . '/app/code/local',
+    $base . '/app/code/community',
+    $base . '/app/code/core',
+    $base . '/lib',
+);
+set_include_path(implode(PATH_SEPARATOR, $paths) . PATH_SEPARATOR . get_include_path());
+unset($paths, $base);
