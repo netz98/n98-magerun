05bed6aa8fafd4d55ef6257f2ebf3fef1fc6df5e
Christian Münch <c.muench@netz98.de>
2013-06-11 11:10:50 +0200

    [TASK] Updated version and phar file
    [ci skip]

--- src/N98/Magento/Application.php
+++ src/N98/Magento/Application.php
@@ -107,7 +107,7 @@ class Application extends BaseApplication
     /**
      * @var string
      */
-    const APP_VERSION = '1.68.1';
+    const APP_VERSION = '1.69.0';
 
     /**
      * @var string
