75141949429dfa7ebde782f682e3bc00b57e8e05
Christian Münch <c.muench@netz98.de>
2013-06-19 11:50:54 +0200

    [BUGFIX] Fixed issue #163 : Missing question mark

--- src/N98/Magento/Application.php
+++ src/N98/Magento/Application.php
@@ -109,7 +109,7 @@ class Application extends BaseApplication
     /**
      * @var string
      */
-    const APP_VERSION = '1.70.1';
+    const APP_VERSION = '1.70.2';
 
     /**
      * @var string
