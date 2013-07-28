76d786f099fd7449a151765a16cd6550581bb09d
Christian Münch <c.muench@netz98.de>
2013-07-28 23:13:12 +0200

    [TASK] Updated dependencies.

--- src/N98/Magento/Application.php
+++ src/N98/Magento/Application.php
@@ -110,7 +110,7 @@ class Application extends BaseApplication
     /**
      * @var string
      */
-    const APP_VERSION = '1.72.0';
+    const APP_VERSION = '1.73.0';
 
     /**
      * @var string
