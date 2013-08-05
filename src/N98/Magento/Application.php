e6e836e95da54cd4cb2a6f8373f9a9750045f1e3
Christian Münch <c.muench@netz98.de>
2013-08-06 00:40:26 +0200

    [TASK] Updated phar file version.

--- src/N98/Magento/Application.php
+++ src/N98/Magento/Application.php
@@ -110,7 +110,7 @@ class Application extends BaseApplication
     /**
      * @var string
      */
-    const APP_VERSION = '1.73.0';
+    const APP_VERSION = '1.73.1';
 
     /**
      * @var string
