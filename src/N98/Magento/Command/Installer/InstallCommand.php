4974b7e8efbe3674f0c692b72eaa2abadb5e5bf6
Christian Münch <c.muench@netz98.de>
2013-08-18 14:13:17 +0200

    [TASK] Tried to make install command compatible for Microsoft Windows.

--- src/N98/Magento/Command/Installer/InstallCommand.php
+++ src/N98/Magento/Command/Installer/InstallCommand.php
@@ -61,14 +61,6 @@ class InstallCommand extends AbstractMagentoCommand
     }
 
     /**
-     * @return bool
-     */
-    public function isEnabled()
-    {
-        return !OperatingSystem::isWindows();
-    }
-
-    /**
      * @param \Symfony\Component\Console\Input\InputInterface $input
      * @param \Symfony\Component\Console\Output\OutputInterface $output
      * @return int|void
@@ -494,8 +486,14 @@ class InstallCommand extends AbstractMagentoCommand
         }
 
         $output->writeln('<info>Start installation process.</info>');
-        $output->writeln('<comment>/usr/bin/env php ' . $this->getInstallScriptPath() . ' ' . $installArgs . '</comment>');
-        exec('/usr/bin/env php ' . $this->getInstallScriptPath() . ' ' . $installArgs);
+
+        if (OperatingSystem::isWindows()) {
+            $installCommand = 'php ' . $this->getInstallScriptPath() . ' ' . $installArgs;
+        } else {
+            $installCommand = '/usr/bin/env php ' . $this->getInstallScriptPath() . ' ' . $installArgs;
+        }
+        $output->writeln('<comment>' . $installCommand . '</comment>');
+        exec($installCommand);
 
         $dialog = $this->getHelperSet()->get('dialog');
 
