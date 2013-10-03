8b93d70c5f50f325fde055313835a949c9f97d06
Christian Münch <c.muench@netz98.de>
2013-10-03 17:12:56 +0200

    [FEATURE] Added new option "only-if-empty" to db:import command.

--- src/N98/Magento/Command/Database/ImportCommand.php
+++ src/N98/Magento/Command/Database/ImportCommand.php
@@ -16,6 +16,7 @@ class ImportCommand extends AbstractDatabaseCommand
             ->addArgument('filename', InputArgument::OPTIONAL, 'Dump filename')
             ->addOption('compression', 'c', InputOption::VALUE_REQUIRED, 'The compression of the specified file')
             ->addOption('only-command', null, InputOption::VALUE_NONE, 'Print only mysql command. Do not execute')
+            ->addOption('only-if-empty', null, InputOption::VALUE_NONE, 'Imports only if database is empty')
             ->setDescription('Imports database with mysql cli client according to database defined in local.xml');
     }
 
@@ -27,14 +28,8 @@ class ImportCommand extends AbstractDatabaseCommand
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $this->detectDbSettings($output);
-
         $this->writeSection($output, 'Import MySQL Database');
-
-        $fileName = $input->getArgument('filename');
-
-        if (!file_exists($fileName)) {
-            throw new \InvalidArgumentException('File does not exist');
-        }
+        $fileName = $this->checkFilename($input);
         
         $compressor = $this->getCompressor($input->getOption('compression'));
 
@@ -47,18 +42,57 @@ class ImportCommand extends AbstractDatabaseCommand
         if ($input->getOption('only-command')) {
             $output->writeln($exec);
         } else {
-            // Import
-            $output->writeln('<comment>Importing SQL dump <info>' . $fileName . '</info> to database <info>' . $this->dbSettings['dbname'] . '</info>');
-            exec($exec, $commandOutput, $returnValue);
-            if ($returnValue > 0) {
-                $output->writeln('<error>' . implode(PHP_EOL, $commandOutput) . '</error>');
+            if ($input->getOption('only-if-empty')
+                && count($this->getHelper('database')->getTables()) > 0
+            ) {
+                $output->writeln('<comment>Skip import. Database is not empty</comment>');
+
+                return;
             }
-            $output->writeln('<info>Finished</info>');
+
+            $this->doImport($output, $fileName, $exec);
         }
     }
 
     public function asText() {
         return parent::asText() . "\n" .
             $this->getCompressionHelp();
+    }
+
+    /**
+     * @param InputInterface $input
+     *
+     * @return mixed
+     * @throws \InvalidArgumentException
+     */
+    protected function checkFilename(InputInterface $input)
+    {
+        $fileName = $input->getArgument('filename');
+        if (!file_exists($fileName)) {
+            throw new \InvalidArgumentException('File does not exist');
+        }
+        return $fileName;
+    }
+
+    /**
+     * @param OutputInterface $output
+     * @param string          $fileName
+     * @param string          $exec
+     *
+     * @return void
+     */
+    protected function doImport(OutputInterface $output, $fileName, $exec)
+    {
+        $returnValue = null;
+        $commandOutput = null;
+        $output->writeln(
+            '<comment>Importing SQL dump <info>' . $fileName . '</info> to database <info>'
+            . $this->dbSettings['dbname'] . '</info>'
+        );
+        exec($exec, $commandOutput, $returnValue);
+        if ($returnValue > 0) {
+            $output->writeln('<error>' . implode(PHP_EOL, $commandOutput) . '</error>');
+        }
+        $output->writeln('<info>Finished</info>');
     }
 }
