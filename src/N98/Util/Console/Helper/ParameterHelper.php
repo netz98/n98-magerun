5ec5385a5cb66de1c1ad3d8fec8c38c9ee9637e2
Christian Münch <c.muench@netz98.de>
2013-10-25 15:19:18 +0200

    [TASK] Issue #238 - Prompt only if there are more than one store view available.

--- src/N98/Util/Console/Helper/ParameterHelper.php
+++ src/N98/Util/Console/Helper/ParameterHelper.php
@@ -60,15 +60,20 @@ class ParameterHelper extends AbstractHelper
                 $question[] = '<comment>[' . ($i + 1) . ']</comment> ' . $store->getCode() . ' - ' . $store->getName() . PHP_EOL;
                 $i++;
             }
-            $question[] = '<question>Please select a store: </question>';
 
-            $storeId = $this->getHelperSet()->get('dialog')->askAndValidate($output, $question, function($typeInput) use ($stores) {
-                if (!isset($stores[$typeInput - 1])) {
-                    throw new \InvalidArgumentException('Invalid store');
-                }
+            if (count($stores) > 1) {
+                $question[] = '<question>Please select a store: </question>';
+                $storeId = $this->getHelperSet()->get('dialog')->askAndValidate($output, $question, function($typeInput) use ($stores) {
+                    if (!isset($stores[$typeInput - 1])) {
+                        throw new \InvalidArgumentException('Invalid store');
+                    }
 
-                return $stores[$typeInput - 1];
-            });
+                    return $stores[$typeInput - 1];
+                });
+            } else {
+                // only one store view available -> take it
+                $storeId = $stores[0];
+            }
 
             $store = \Mage::app()->getStore($storeId);
         }
