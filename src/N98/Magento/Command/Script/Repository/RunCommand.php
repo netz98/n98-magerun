012b5e49f8286afadd5dda6d2d219b81eb749edb
Christian Münch <c.muench@netz98.de>
2014-03-09 15:42:28 +0100

    [~TASK] #268 script:repo Handling of *.magerun files

--- src/N98/Magento/Command/Script/Repository/RunCommand.php
+++ src/N98/Magento/Command/Script/Repository/RunCommand.php
@@ -12,12 +12,37 @@ class RunCommand extends AbstractRepositoryCommand
 {
     protected function configure()
     {
+        $help = <<<HELP
+Please note that the script repo command runs only scripts which are stored
+in a defined script folder.
+
+Script folders can defined by config.
+
+Example:
+
+script:
+  folders:
+    - /my/script_folder
+
+
+There are some pre defined script folders:
+
+- /usr/local/share/n98-magerun/scripts
+- ~/.n98-magerun/scripts
+
+If you like to run a standalone script you can also use the "script" command.
+
+See: n98-magerun.phar script <filename.magerun>
+
+HELP;
+
         $this
             ->setName('script:repo:run')
             ->addArgument('script', InputArgument::OPTIONAL, 'Name of script in repository')
             ->addOption('define', 'd', InputOption::VALUE_OPTIONAL | InputOption::VALUE_IS_ARRAY, 'Defines a variable')
             ->addOption('stop-on-error', null, InputOption::VALUE_NONE, 'Stops execution of script on error')
             ->setDescription('Run script from repository')
+            ->setHelp($help)
         ;
     }
 
