e0ef7c6e07cde72be97bb03a3db831d72c0d97fe
Christian Münch <c.muench@netz98.de>
2014-05-23 18:14:47 +0200

    [~BUGFIX] Removed duplicate helper class registration.

--- src/N98/Magento/Command/AbstractMagentoCommand.php
+++ src/N98/Magento/Command/AbstractMagentoCommand.php
@@ -2,7 +2,6 @@
 
 namespace N98\Magento\Command;
 
-use N98\Util\Console\Helper\MagentoHelper;
 use Composer\Package\PackageInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
