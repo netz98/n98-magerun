2c29e7da09d0f0d6f7f8abfa3308e06c4e2807d9
Steve Robbins <steven.j.robbins@gmail.com>
2014-10-27 15:36:52 -0700

    enable or disable a module based on name or codepool

---
+++ src/N98/Magento/Command/Developer/Module/Disableenable/EnableCommand.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace N98\Magento\Command\Developer\Module\Disableenable;
+
+/**
+ * Enable Magento module(s)
+ */
+class EnableCommand extends AbstractCommand
+{
+    /**
+     * @var string
+     */
+    protected $commandName = 'enable';
+}
