122e4b818fe9d327a27a89ec1e80f6f74d52e1c3
Alexander Menk <a.menk@imi.de>
2015-04-28 10:52:26 +0200

    #532 Optimization fixes and improvements
    - Optimized dump was ignored, instead the original was imported
    - dump was broken if a field value contained the work "values"
    - added autocommit=0 optimization

--- src/N98/Magento/Command/Database/ImportCommand.php
+++ src/N98/Magento/Command/Database/ImportCommand.php
@@ -50,12 +50,13 @@ HELP;
         $result = tempnam(sys_get_temp_dir(), 'dump') . '.sql';
         $out = fopen($result, 'w');
 
+        fwrite($out, 'SET autocommit=0;' . "\n");
         $currentTable = '';
         $maxlen = 8 * 1024 * 1024; // 8 MB
         $len = 0;
         while ($line = fgets($in)) {
             if (strtolower(substr($line, 0, 11)) == 'insert into') {
-                preg_match('/^insert into `(.*)` \(.*\) values (.*);/i', $line, $m);
+                preg_match('/^insert into `(.*)` \([^)]*\) values (.*);/i', $line, $m);
 
                 if (count($m) < 3) { // fallback for very long lines or other cases where the preg_match fails
                     if ($currentTable != '') {
@@ -71,7 +72,7 @@ HELP;
 
                 if ($table != $currentTable or ($len > $maxlen - 1000)) {
                     if ($currentTable != '') {
-                        fwrite($out, ";\n\n");
+                        fwrite($out, ";\n");
                     }
                     $currentTable = $table;
                     $insert = 'INSERT INTO `' . $table . '` VALUES ' . $values;
@@ -90,6 +91,11 @@ HELP;
             }
 
         }
+
+        fwrite($out, ";\n");
+
+        fwrite($out, 'COMMIT;' . "\n");
+
         fclose($in);
         fclose($out);
 
@@ -111,6 +117,17 @@ HELP;
 
         $compressor = $this->getCompressor($input->getOption('compression'));
 
+        if ($input->getOption('optimize')) {
+            if ($input->getOption('only-command')) {
+                throw new \InvalidArgumentException('Options --only-command and --optimize are not compatible');
+            }
+            if ($input->getOption('compression')) {
+                throw new \Exception('Options --compression and --optimize are not compatible');
+            }
+            $output->writeln('<comment>Optimizing <info>' . $fileName . '</info> to temporary file');
+            $fileName = $this->optimize($fileName);
+        }
+
         // create import command
         $exec = $compressor->getDecompressingCommand(
             'mysql ' . $dbHelper->getMysqlClientToolConnectionString(),
@@ -118,7 +135,6 @@ HELP;
         );
         if ($input->getOption('only-command')) {
             $output->writeln($exec);
-
             return;
         } else {
             if ($input->getOption('only-if-empty')
@@ -130,14 +146,6 @@ HELP;
             }
         }
 
-        if ($input->getOption('optimize')) {
-            if ($input->getOption('compression')) {
-                throw new \Exception('Options --compression and --optimize are not compatible');
-            }
-            $output->writeln('<comment>Optimizing <info>' . $fileName . '</info> to temporary file');
-            $fileName = $this->optimize($fileName);
-        }
-
         if( $input->getOption('drop') ) {
             $dbHelper->dropDatabase($output);
             $dbHelper->createDatabase($output);
@@ -146,10 +154,7 @@ HELP;
             $dbHelper->dropTables($output);
         }
 
-
-
-
-            $this->doImport($output, $fileName, $exec);
+        $this->doImport($output, $fileName, $exec);
 
         if ($input->getOption('optimize')) {
             unlink($fileName);
