50af8dbbbdb19cd156d177b52c607bc007afd6e1
Tom Klingenberg <tklingenberg@lastflood.net>
2015-08-30 22:19:49 +0200

    [FIX] fix varhint (Abstract)EntityType
    the protected field $attribute was wrongly hinted as string.
    
    this has been fixed.
    
    the interface didn't typehint, too, on the ctor. this has been fixed.
    
    additionally the class is not imported any longer as it is a magento core
    type.

--- src/N98/Magento/Command/Developer/Setup/Script/Attribute/EntityType/EntityType.php
+++ src/N98/Magento/Command/Developer/Setup/Script/Attribute/EntityType/EntityType.php
@@ -5,9 +5,9 @@ namespace N98\Magento\Command\Developer\Setup\Script\Attribute\EntityType;
 interface EntityType
 {
     /**
-     * @param \Mage_Eav_Model_Entity_Attribute $attributeCode
+     * @param \Mage_Eav_Model_Entity_Attribute $attribute
      */
-    public function __construct($attributeCode);
+    public function __construct(\Mage_Eav_Model_Entity_Attribute $attribute);
     public function setReadConnection($connection);
     public function getWarnings();
     public function generateCode();
