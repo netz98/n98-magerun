1fea4ca9ca0cba4ff1d228f028ec471a3c3b038f
Tom Klingenberg <tklingenberg@lastflood.net>
2015-09-04 20:49:31 +0200

    [TASK] init Circleci
    Run the testsuite on Circleci with Xdebug enabled to get code-coverage
    data on the latest Magento CE and highest to-be-supported PHP version
    incl. it's over-large sample-data as Circleci caches this as dependencies.
    
    Make the magento installment a dependency instead of a test (this was the
    case on travis before). This allows circleci to automatically cache most
    of the assets.
    
    Reference:
    
      Slow Unit-Tests because of Code-Coverage / XDebug
      https://github.com/netz98/n98-magerun/issues/670

---
+++ build/circleci/test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+. build/circleci/source.sh
+
+buildecho "php version:"
+php --version
+
+export N98_MAGERUN_TEST_MAGENTO_ROOT="./${MAGENTO_VERSION}"
+buildecho "magento test root '${N98_MAGERUN_TEST_MAGENTO_ROOT}' exported as \$N98_MAGERUN_TEST_MAGENTO_ROOT."
+
+buildecho "run magerun phpunit testsuite:"
+php -f vendor/phpunit/phpunit/phpunit -- --coverage-clover "${CLOVER_XML}"  \
+        --log-junit "${CIRCLE_TEST_REPORTS}/junit/junit.xml"
+
+buildecho "check coverage percentage:"
+php -f tests/check-coverage.php -- "${CLOVER_XML}" "${COVERAGE}"
