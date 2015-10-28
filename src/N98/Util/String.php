643cbed8dacdb40907403f57d268daf97746070a
Tom Klingenberg <tklingenberg@lastflood.net>
2015-10-28 13:26:03 +0100

    [TASK] #691 PHP 7 backward compat
    changes in 2c4a331 are not backwards compatible so would break on minor
    release.
    
    this commit adds the String utility class again ensuring that potential
    magerun extensions do not break.

---
+++ src/N98/Util/String.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace N98\Util;
+
+/**
+ * Class String
+ *
+ * This class is PHP 7 incompatible which is why it has been renamed to BinaryString
+ *
+ * @deprecated 1.97.6
+ *
+ * @package N98\Util
+ */
+class String extends BinaryString
+{
+}
