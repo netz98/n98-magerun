63f06b803c7f72a915ef06e21b5d200c9e0fafbb
Ben Robie <brobie@degdigital.com>
2016-01-30 20:56:03 -0600

    Adding a prompt when unlocking all admin users.

--- src/N98/Magento/Command/Admin/User/UnlockCommand.php
+++ src/N98/Magento/Command/Admin/User/UnlockCommand.php
@@ -33,7 +33,7 @@ class UnlockCommand extends AbstractAdminUserCommand
      * @param InputInterface  $input
      * @param OutputInterface $output
      *
-*@return void
+     *@return void
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
@@ -49,12 +49,15 @@ class UnlockCommand extends AbstractAdminUserCommand
                 $output->writeln('<info><comment>' . $username . '</comment> unlocked</info>');
                 return;
             }
-            \Mage::getResourceModel('enterprise_pci/admin_user')->unlock(
-                \Mage::getModel('admin/user')
-                    ->getCollection()
-                    ->getAllIds()
-            );
-            $output->writeln('<info><comment>All admins</comment> unlocked</info>');
+            $dialog = $this->getHelperSet()->get('dialog');
+            $shouldUnlockAll = $dialog->askConfirmation($output, '<question>Really unlock all users?</question> <comment>[n]</comment>: ', false);
+            if ($shouldUnlockAll) {
+                $ids = \Mage::getModel('admin/user')->getCollection()->getAllIds();
+                if (count($ids) > 0) {
+                    \Mage::getResourceModel('enterprise_pci/admin_user')->unlock($ids);
+                }
+                $output->writeln('<info><comment>All admins</comment> unlocked</info>');
+            }
         }
     }
 }
---
+++ tests/N98/Magento/Command/Admin/User/UnlockUserCommandTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace N98\Magento\Command\Admin\User;
+
+use N98\Magento\Command\PHPUnit\TestCase;
+use Symfony\Component\Console\Tester\CommandTester;
+
+/**
+ * Class UnlockUserCommandTest
+ */
+class UnlockUserCommandTest extends TestCase
+{
+
+
+
+    public function testUnlockAllUsersPromptNo()
+    {
+        $dialog = $this->getMock('Symfony\Component\Console\Helper\DialogHelper', array('ask'));
+
+        $dialog->expects($this->once())
+            ->method('ask')
+            ->will($this->returnValue("n"));
+
+        $application = $this->getApplication();
+        $application->add(new UnlockCommand());
+        $command = $this->getApplication()->find('admin:user:unlock');
+        $command->getHelperSet()->set($dialog, 'dialog');
+
+        $commandTester = new CommandTester($command);
+        $commandTester->execute(array('command' => $command->getName()));
+
+        $this->assertNotContains('All admins unlocked', $commandTester->getDisplay());
+    }
+
+    public function testUnlockAllUsersPromptYes()
+    {
+        $dialog = $this->getMock('Symfony\Component\Console\Helper\DialogHelper', array('ask'));
+
+        $dialog->expects($this->once())
+            ->method('ask')
+            ->will($this->returnValue("y"));
+
+        $application = $this->getApplication();
+        $application->add(new UnlockCommand());
+        $command = $this->getApplication()->find('admin:user:unlock');
+        $command->getHelperSet()->set($dialog, 'dialog');
+
+        $commandTester = new CommandTester($command);
+        $commandTester->execute(array('command' => $command->getName()));
+
+        $this->assertContains('All admins unlocked', $commandTester->getDisplay());
+    }
+}
