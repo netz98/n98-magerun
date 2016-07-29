f583d9334c3d6183b718578f23fdbd6d29f22d39
Tom Klingenberg <tklingenberg@lastflood.net>
2016-07-29 19:52:24 +0200

    git subrepo pull shared
    subrepo:
      subdir:   "shared"
      merged:   "0212215"
    upstream:
      origin:   "../mr-shared"
      branch:   "master"
      commit:   "0212215"
    git-subrepo:
      version:  "0.3.0"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "cb2995b"

---
+++ shared/src/N98/MagerunBootstrap.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * this file is part of magerun-shared
+ *
+ * @author Tom Klingenberg <https://github.com/ktomk>
+ */
+namespace N98;
+
+use Composer\Autoload\ClassLoader;
+use ErrorException;
+
+/**
+ * Bootstrap class for the Magerun applications (Symfony console based application)
+ *
+ * @package N98
+ */
+class MagerunBootstrap
+{
+    /**
+     * @param ClassLoader|null $loader [optional]
+     * @return Magento\Application
+     * @throws ErrorException
+     */
+    public static function createApplication(ClassLoader $loader = null)
+    {
+        if (null === $loader) {
+            $loader = self::getLoader();
+        }
+
+        $application = new Magento\Application($loader);
+
+        return $application;
+    }
+
+    /**
+     * @throws ErrorException
+     * @return ClassLoader
+     */
+    public static function getLoader()
+    {
+        $projectBasedir = __DIR__ . '/../../..';
+        if (
+            !($loader = self::includeIfExists($projectBasedir . '/vendor/autoload.php'))
+            && !($loader = self::includeIfExists($projectBasedir . '/../../autoload.php'))
+        ) {
+            throw new ErrorException(
+                'You must set up the project dependencies, run the following commands:' . PHP_EOL .
+                'curl -s http://getcomposer.org/installer | php' . PHP_EOL .
+                'php composer.phar install' . PHP_EOL
+            );
+        }
+
+        return $loader;
+    }
+
+    /**
+     * @param string $file
+     * @return mixed
+     */
+    public static function includeIfExists($file)
+    {
+        if (file_exists($file)) {
+            return include $file;
+        }
+    }
+}
