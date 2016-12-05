493cf9ca9dff10df0d693ac3be8369490096570f
Tom Klingenberg <tklingenberg@lastflood.net>
2016-12-05 10:06:56 +0100

    Fix bash script styles
    Indentation to two-space indents, if bracket style.
    
    Refs:
    
    - https://google.github.io/styleguide/shell.xml

--- build.sh
+++ build.sh
@@ -31,19 +31,19 @@ finish() {
 }
 trap finish EXIT
 
-if [ -d "${build_dir}" ]; then
-    rm -rf "${build_dir}"
+if [[ -d "${build_dir}" ]]; then
+  rm -rf "${build_dir}"
 fi
-if [ -d "${build_dir}" ]; then
-    echo "Can not remove build-dir '${build_dir}'"
-    echo "aborting."
-    exit 1
+if [[ -d "${build_dir}" ]]; then
+  >&2 echo "Error: Can not remove build-dir '${build_dir}'"
+  echo "aborting."
+  exit 1
 fi
 mkdir "${build_dir}"
-if [ ! -d "${build_dir}" ]; then
-    echo "Can not create build-dir '${build_dir}'"
-    echo "aborting."
-    exit 1
+if [[ ! -d "${build_dir}" ]]; then
+  >&2 echo "Error: Can not create build-dir '${build_dir}'"
+  echo "aborting."
+  exit 1
 fi
 
 git clone --quiet --no-local -- . "${build_dir}"
@@ -52,22 +52,22 @@ remove_assume_unchanged "${build_dir}" "n98-magerun.phar"
 
 composer="${build_dir}/composer.phar"
 
-if [ -e "${composer}" ]; then
-    rm "${composer}"
+if [[ -e "${composer}" ]]; then
+  rm "${composer}"
 fi
 
 # Set COMPOSER_HOME if HOME and COMPOSER_HOME not set (shell with no home-dir, e.g. build server with webhook)
-if [ -z ${HOME+x} ]; then
-    if [ -z ${COMPOSER_HOME+x} ]; then
-        mkdir -p "build/composer-home"
-        export COMPOSER_HOME="$(pwd -P)/build/composer-home"
-    fi
+if [[ -z ${HOME+x} ]]; then
+  if [ -z ${COMPOSER_HOME+x} ]; then
+    mkdir -p "build/composer-home"
+    export COMPOSER_HOME="$(pwd -P)/build/composer-home"
+  fi
 fi
 
-if [ ! -e "${composer}" ]; then
-    echo "Downloading composer.phar..."
-    wget --quiet -O "${composer}" https://getcomposer.org/download/1.1.3/composer.phar
-    chmod +x "${composer}"
+if [[ ! -e "${composer}" ]]; then
+  echo "Downloading composer.phar..."
+  wget --quiet -O "${composer}" https://getcomposer.org/download/1.1.3/composer.phar
+  chmod +x "${composer}"
 fi
 
 "${composer}" --version
@@ -75,17 +75,17 @@ php --version
 
 echo "Composer install in '${build_dir}'..."
 if ! "${composer}" -d="${build_dir}" --profile -q install --no-dev --no-interaction; then
-    echo "failed to install from composer.lock, installing without lockfile now"
-    rm "${build_dir}"/composer.lock
-    "${composer}" -d="${build_dir}" --profile -q install --no-dev --no-interaction
+  echo "failed to install from composer.lock, installing without lockfile now"
+  rm "${build_dir}"/composer.lock
+  "${composer}" -d="${build_dir}" --profile -q install --no-dev --no-interaction
 fi
 
 echo "Composer install build requirements in '${build_dir}/build'..."
 "${composer}" -d="${build_dir}"/build --profile -q install --no-interaction
 
-if [ -e "${phar}" ]; then
-    echo "Remove earlier created ${phar} file"
-    rm "${phar}"
+if [[ -e "${phar}" ]]; then
+  echo "Remove earlier created ${phar} file"
+  rm "${phar}"
 fi
 
 cd "${build_dir}"
@@ -101,9 +101,10 @@ set +e
 php -f build/vendor/phing/phing/bin/phing -dphar.readonly=0 -- dist_clean
 BUILD_STATUS=$?
 set -e
-if [ ${BUILD_STATUS} -ne 0 ]; then
-    >&2 echo "error: phing build failed with exit status ${BUILD_STATUS}"
-    exit ${BUILD_STATUS}
+if [[ ${BUILD_STATUS} -ne 0 ]]; then
+  >&2 echo "error: phing build failed with exit status ${BUILD_STATUS}"
+  echo "aborting."
+  exit ${BUILD_STATUS}
 fi
 
 php -f build/phar/phar-timestamp.php
