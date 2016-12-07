netz98/n98-magerun

commit e247690d15e6de433f61e270710d48cc491b18ba

Cleanup output directory if build fails
Exit trap that removes the build-dir again (in case the build script
exits early).

Additionally abort if the build-dir can not be initially removed.

build.sh
@@ -26,12 +26,18 @@ remove_assume_unchanged "." "${phar}"
 echo "$0 executed in $(pwd -P)"
 
 build_dir="build/output"
+finish() {
+  rm -rf "${build_dir}"
+}
+trap finish EXIT
 
 if [ -d "${build_dir}" ]; then
     rm -rf "${build_dir}"
 fi
 if [ -d "${build_dir}" ]; then
     echo "Can not remove build-dir '${build_dir}'"
+    echo "aborting."
+    exit 1
 fi
 mkdir "${build_dir}"
 if [ ! -d "${build_dir}" ]; then

commit 743ffd7fa61f545fbf8ac55a33403985e937f0c0

Fix cleanup output directory if build fails
In e247690 the trap was introduced however it did not operate on the
full path and as the path changes while the build is running under
circumstances it did not remove the output directory.

Fix is to store the absolute base directory for the build in another
variable so that the absolute full output path can be removed.

Refs:

- e247690 Cleanup output directory if build fails

build.sh
@@ -23,11 +23,13 @@ echo "Building ${phar}..."
 
 remove_assume_unchanged "." "${phar}"
 
-echo "$0 executed in $(pwd -P)"
+base_dir="$(pwd -P)"
+echo "$0 executed in ${base_dir}"
 
 build_dir="build/output"
 finish() {
-  rm -rf "${build_dir}"
+  echo "trap exit removing '${build_dir}'.."
+  rm -rf "${base_dir}/${build_dir}"
 }
 trap finish EXIT
 