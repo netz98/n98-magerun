a5a7171e9a6f112146d70b7cfcc6ee9b3a39f185
Christian Münch <c.muench@netz98.de>
2017-07-28 18:24:08 +0200

    [TASK] Add compatibility for stores with symfony 3 components
    If a store uses composer components and loads symfony 3 we have
    a clash with the missing deprecated dispatcher functions.

--- src/N98/Magento/Application/Console/Event.php
+++ src/N98/Magento/Application/Console/Event.php
@@ -6,6 +6,7 @@ use N98\Magento\Application;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\EventDispatcher\Event as BaseEvent;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
 class Event extends BaseEvent
 {
@@ -23,6 +24,11 @@ class Event extends BaseEvent
      * @var OutputInterface
      */
     protected $output;
+
+    /**
+     * @var EventDispatcherInterface Dispatcher that dispatched this event
+     */
+    private $dispatcher;
 
     public function __construct(Application $application, InputInterface $input, OutputInterface $output)
     {
@@ -57,5 +63,29 @@ class Event extends BaseEvent
     public function getApplication()
     {
         return $this->application;
+    }
+
+    /**
+     * Stores the EventDispatcher that dispatches this Event.
+     *
+     * @param EventDispatcherInterface $dispatcher
+     *
+     * @deprecated since version 2.4, to be removed in 3.0. The event dispatcher is passed to the listener call.
+     */
+    public function setDispatcher(EventDispatcherInterface $dispatcher)
+    {
+        $this->dispatcher = $dispatcher;
+    }
+
+    /**
+     * Returns the EventDispatcher that dispatches this Event.
+     *
+     * @return EventDispatcherInterface
+     *
+     * @deprecated since version 2.4, to be removed in 3.0. The event dispatcher is passed to the listener call.
+     */
+    public function getDispatcher()
+    {
+        return $this->dispatcher;
     }
 }
