ebedd01fe3cfaba6f29e6810c5f63f25acdc1063
Simon Sprankel <simonsprankel@gmail.com>
2017-10-14 15:16:32 +0200

    Add Magento CE 1.9.3.6

--- src/N98/Magento/Command/Installer/InstallCommand.php
+++ src/N98/Magento/Command/Installer/InstallCommand.php
@@ -119,12 +119,12 @@ class InstallCommand extends AbstractMagentoCommand
 * Starts Magento installer
 * Sets rewrite base in .htaccess file
 
-Example of an unattended Magento CE/ Open Source 1.9.3.4 installation:
+Example of an unattended Magento CE/ Open Source 1.9.3.6 installation:
 
    $ n98-magerun.phar install --dbHost="localhost" --dbUser="mydbuser" \
      --dbPass="mysecret" --dbName="magentodb" --installSampleData=yes \
      --useDefaultConfigParams=yes \
-     --magentoVersionByName="magento-mirror-1.9.3.4" \
+     --magentoVersionByName="magento-mirror-1.9.3.6" \
      --installationFolder="magento" --baseUrl="http://magento.localdomain/"
 
 (Magento is only freely available via Github with Magerun, it uses the best
