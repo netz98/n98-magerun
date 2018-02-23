netz98/n98-magerun

commit c51e27a499e8e5353d239be378247dd645429393

Add README

src/N98/Magento/Command/System/Cron/RunCommand.php
@@ -32,6 +32,7 @@ class RunCommand extends AbstractCronCommand
         $help = <<<HELP
 If no `job` argument is passed you can select a job from a list.
 See it in action: http://www.youtube.com/watch?v=QkzkLgrfNaM
+If option schedule is present, cron is not launched, but just scheduled immediately in magento crontab.
 HELP;
         $this->setHelp($help);
     }

commit 5a8a6aca8bdef36fb4b2e23af6259249ef3cdaad

Fix immediate execution of sys:cron:run This patches fixes an issue introduced in #966 with the --schedule command switch. The hasOption method is always returning true which causes commands to always be scheduled instead of running immediately as default. The executeConfigModel and scheduleConfigModel was also changed in order to avoid the double negation of hasOption and getOption which would make it a bit harder to understand. Also, having hasOption && getOption keeps it consistent with how other commands operate (e.g. cache:view)\

src/N98/Magento/Command/System/Cron/RunCommand.php
@@ -62,10 +62,10 @@ HELP;
 
         $output->write('<info>Run </info><comment>' . $callableName . '</comment> ');
 
-        if (!$input->hasOption('schedule')) {
-            $this->executeConfigModel($callback, $jobCode);
-        } else {
+        if ($input->hasOption('schedule') && $input->getOption('schedule')) {
             $this->scheduleConfigModel($callback, $jobCode);
+        } else {
+            $this->executeConfigModel($callback, $jobCode);
         }
 
         $output->writeln('<info>done</info>');