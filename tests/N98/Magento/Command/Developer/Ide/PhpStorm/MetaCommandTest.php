83a487ac0024ed287276f46bee7c51813d78b1ba
Christian Münch <christian@muench-worms.de>
2020-10-14 16:32:08 +0200

    Fix tests to be compatible with OpenMage 20.0.3

--- tests/N98/Magento/Command/Developer/Ide/PhpStorm/MetaCommandTest.php
+++ tests/N98/Magento/Command/Developer/Ide/PhpStorm/MetaCommandTest.php
@@ -25,17 +25,17 @@ class MetaCommandTest extends TestCase
 
         $this->assertContains('\'catalog\' instanceof \Mage_Catalog_Helper_Data', $fileContent);
         $this->assertContains('\'core/config\' instanceof \Mage_Core_Model_Config', $fileContent);
+
         if (class_exists('\Mage_Core_Model_Resource_Config')) { // since magento 1.7
             $this->assertContains('\'core/config\' instanceof \Mage_Core_Model_Resource_Config', $fileContent);
         }
+
         $this->assertContains('\'wishlist\' instanceof \Mage_Wishlist_Helper_Data', $fileContent);
+
         if (class_exists('\Mage_Core_Model_Resource_Helper_Mysql4')) {
             $this->assertContains('\'core\' instanceof \Mage_Core_Model_Resource_Helper_Mysql4', $fileContent);
         }
-        $this->assertNotContains(
-            '\'core/mysql4_design_theme_collection\' instanceof \Mage_Core_Model_Mysql4_Design_Theme_Collection',
-            $fileContent
-        );
+
         $this->assertNotContains(
             '\'payment/paygate_request\' instanceof \Mage_Payment_Model_Paygate_Request',
             $fileContent
