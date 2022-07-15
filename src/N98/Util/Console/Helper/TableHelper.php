a02b53079e606a46a7d9491bb86cb23fd6e15326
Christian Münch <christian@muench-worms.de>
2022-07-15 15:40:14 +0200

    Backport TableHelper from n98-magerun2

--- src/N98/Util/Console/Helper/TableHelper.php
+++ src/N98/Util/Console/Helper/TableHelper.php
@@ -4,7 +4,8 @@ namespace N98\Util\Console\Helper;
 
 use N98\Util\Console\Helper\Table\Renderer\RendererFactory;
 use N98\Util\Console\Helper\Table\Renderer\RendererInterface;
-use Symfony\Component\Console\Helper\TableHelper as BaseTableHelper;
+use Symfony\Component\Console\Helper\Helper as AbstractHelper;
+use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Output\OutputInterface;
 
 /**
@@ -14,7 +15,7 @@ use Symfony\Component\Console\Output\OutputInterface;
  * Based on draw_text_table by Paul Maunders
  * Available at http://www.pyrosoft.co.uk/blog/2007/07/01/php-array-to-text-table-function/
  */
-class TableHelper extends BaseTableHelper
+class TableHelper extends AbstractHelper
 {
     /**
      * @var string
@@ -24,7 +25,12 @@ class TableHelper extends BaseTableHelper
     /**
      * @var array
      */
-    protected $headers = array();
+    protected $headers = [];
+
+    /**
+     * @var array
+     */
+    protected $rows = [];
 
     /**
      * @param string $format
@@ -46,30 +52,6 @@ class TableHelper extends BaseTableHelper
     }
 
     /**
-     * @param OutputInterface $output
-     */
-    public function render(OutputInterface $output)
-    {
-        if ($this->format == 'csv') {
-            $this->renderCsv();
-        } else {
-            parent::render($output);
-        }
-    }
-
-    /**
-     * @param array|string[] $headers
-     * @return $this
-     */
-    public function setHeaders(array $headers)
-    {
-        $this->headers = array_values($headers);
-        parent::setHeaders($headers);
-
-        return $this;
-    }
-
-    /**
      * @param OutputInterface $outputInterface
      * @param array $rows
      * @param string $format [optional]
@@ -78,10 +60,14 @@ class TableHelper extends BaseTableHelper
     {
         $rendererFactory = new RendererFactory();
         $renderer = $rendererFactory->create($format);
+
         if ($renderer && $renderer instanceof RendererInterface) {
             foreach ($rows as &$row) {
-                $row = array_combine($this->headers, $row);
+                if (!empty($this->headers)) {
+                    $row = array_combine($this->headers, $row);
+                }
             }
+
             $renderer->render($outputInterface, $rows);
         } else {
             $this->setRows($rows);
@@ -102,5 +88,51 @@ class TableHelper extends BaseTableHelper
         $this->setHeaders(array_keys($rows[0]));
         $this->setRows($rows);
         $this->render($output);
+    }
+
+    /**
+     * @param OutputInterface $output
+     * @param array $rows
+     */
+    public function render(OutputInterface $output, $rows = [])
+    {
+        if (empty($rows)) {
+            $rows = $this->rows;
+        }
+
+        $baseTable = new Table($output);
+        $baseTable->setRows($rows);
+        $baseTable->setHeaders($this->headers);
+        $baseTable->render();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getName()
+    {
+        return 'table';
+    }
+
+    /**
+     * @param array $rows
+     * @return $this
+     */
+    public function setRows(array $rows)
+    {
+        $this->rows = $rows;
+
+        return $this;
+    }
+
+    /**
+     * @param array|string[] $headers
+     * @return $this
+     */
+    public function setHeaders(array $headers)
+    {
+        $this->headers = array_values($headers);
+
+        return $this;
     }
 }
