e19547e844cd739fce7b005b58756d961d972784
Christian Münch <christian@muench-worms.de>
2023-05-20 17:57:25 +0200

    Backport version file printer from Magerun 2

---
+++ src/N98/Util/Markdown/VersionFilePrinter.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace N98\Util\Markdown;
+
+/**
+ * Class VersionFilePrinter
+ * @package N98\Util\Markdown
+ */
+class VersionFilePrinter
+{
+    /**
+     * @var string
+     */
+    private $content;
+
+    /**
+     * @param string $content
+     */
+    public function __construct($content)
+    {
+        $this->content = $content;
+    }
+
+    /**
+     * @param string $startVersion
+     * @return string
+     */
+    public function printFromVersion($startVersion)
+    {
+        $contentToReturn = '';
+
+        $lines = preg_split("/((\r?\n)|(\r\n?))/", $this->content);
+
+        foreach ($lines as $line) {
+            if ($line === $startVersion) {
+                break;
+            }
+
+            $contentToReturn .= $line . "\n";
+        }
+
+        return trim($contentToReturn) . "\n";
+    }
+}
