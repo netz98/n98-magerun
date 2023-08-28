7a2ec3ef3660ad526ee33f77b267242037ec26d6
Christian Münch <c.muench@netz98.de>
2023-08-28 23:05:04 +0200

    Fix path to phar timestamp tool

--- build.sh
+++ build.sh
@@ -112,7 +112,7 @@ php -f "${phing_bin}" -dphar.readonly=0 -- \
   dist_clean
 
 LAST_COMMIT_TIMESTAMP="$(git log --format=format:%ct HEAD -1)"
-php -f phar-timestamp.php -- $LAST_COMMIT_TIMESTAMP
+php -f build/phar/phar-timestamp.php -- $LAST_COMMIT_TIMESTAMP
 
 php -f "${phar}" -- --version
 ls -al "${phar}"
