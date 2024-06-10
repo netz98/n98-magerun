f7404df5e02b0cb0947ffa55251d0aa115fbe2f6
Sven Reichel <github-sr@hotmail.com>
2024-06-10 06:53:46 +0200

    [skip ci] Updated customer:list command

--- src/N98/Magento/Command/Customer/ListCommand.php
+++ src/N98/Magento/Command/Customer/ListCommand.php
@@ -1,77 +1,88 @@
 <?php
 
+declare(strict_types=1);
+
 namespace N98\Magento\Command\Customer;
 
-use N98\Util\Console\Helper\Table\Renderer\RendererFactory;
-use N98\Util\Console\Helper\TableHelper;
+use Mage_Core_Exception;
+use N98\Magento\Command\AbstractMagentoCommandFormatInterface;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class ListCommand extends AbstractCustomerCommand
+class ListCommand extends AbstractCustomerCommand implements AbstractMagentoCommandFormatInterface
 {
+    protected const NO_DATA_MESSAGE = 'No customers found';
+
+    /**
+     * @var string
+     * @deprecated with symfony 6.1
+     * @see AsCommand
+     */
+    protected static $defaultName = 'customer:list';
+
+    /**
+     * @var string
+     * @deprecated with symfony 6.1
+     * @see AsCommand
+     */
+    protected static $defaultDescription = 'Lists customers.';
+
     protected function configure()
     {
-        $this
-            ->setName('customer:list')
-            ->addArgument('search', InputArgument::OPTIONAL, 'Search query')
-            ->addOption(
-                'format',
-                null,
-                InputOption::VALUE_OPTIONAL,
-                'Output Format. One of [' . implode(',', RendererFactory::getFormats()) . ']'
-            )
-            ->setDescription('Lists customers')
-        ;
+        $this->addArgument('search', InputArgument::OPTIONAL, 'Search query');
 
-        $help = <<<HELP
+        parent::configure();
+    }
+
+    public function getHelp()
+    {
+        return <<<HELP
 List customers. The output is limited to 1000 (can be changed by overriding config).
-If search parameter is given the customers are filtered (searchs in firstname, lastname and email).
+If search parameter is given the customers are filtered (searches in firstname, lastname and email).
 HELP;
-        $this->setHelp($help);
     }
 
     /**
-     * @param InputInterface $input
-     * @param OutputInterface $output
+     * {@inheritdoc}
+     * @return array<int|string, array<string, string>>
+     * @throws Mage_Core_Exception
      *
-     * @return int
+     * phpcs:disable Generic.CodeAnalysis.UnusedFunctionParameter
      */
-    protected function execute(InputInterface $input, OutputInterface $output): int
+    public function getData(InputInterface $input, OutputInterface $output): array
     {
-        $this->detectMagento($output, true);
-        if (!$this->initMagento()) {
-            return 0;
+        if (is_null($this->data)) {
+            $this->data = [];
+
+            $config = $this->getCommandConfig();
+
+            $collection = $this->getCustomerCollection();
+            $collection->addAttributeToSelect(['entity_id', 'email', 'firstname', 'lastname', 'website_id']);
+
+            if ($input->getArgument('search')) {
+                $collection->addAttributeToFilter(
+                    [
+                        ['attribute' => 'email', 'like' => '%' . $input->getArgument('search') . '%'],
+                        ['attribute' => 'firstname', 'like' => '%' . $input->getArgument('search') . '%'],
+                        ['attribute' => 'lastname', 'like' => '%' . $input->getArgument('search') . '%']
+                    ]
+                );
+            }
+
+            $collection->setPageSize($config['limit']);
+
+            foreach ($collection as $customer) {
+                $this->data[] = [
+                    'id'        => $customer->getId(),
+                    'email'     => $customer->getEmail(),
+                    'firstname' => $customer->getFirstname(),
+                    'lastname'  => $customer->getLastname(),
+                    'website'   => $this->_getWebsiteCodeById($customer->getwebsiteId())
+                ];
+            }
         }
 
-        $config = $this->getCommandConfig();
-
-        $collection = $this->getCustomerCollection();
-        $collection->addAttributeToSelect(['entity_id', 'email', 'firstname', 'lastname', 'website_id']);
-
-        if ($input->getArgument('search')) {
-            $collection->addAttributeToFilter(
-                [['attribute' => 'email', 'like' => '%' . $input->getArgument('search') . '%'], ['attribute' => 'firstname', 'like' => '%' . $input->getArgument('search') . '%'], ['attribute' => 'lastname', 'like' => '%' . $input->getArgument('search') . '%']]
-            );
-        }
-
-        $collection->setPageSize($config['limit']);
-
-        $table = [];
-        foreach ($collection as $customer) {
-            $table[] = [$customer->getId(), $customer->getEmail(), $customer->getFirstname(), $customer->getLastname(), $this->_getWebsiteCodeById($customer->getwebsiteId())];
-        }
-
-        if (count($table) > 0) {
-            /* @var TableHelper $tableHelper */
-            $tableHelper = $this->getHelper('table');
-            $tableHelper
-                ->setHeaders(['id', 'email', 'firstname', 'lastname', 'website'])
-                ->renderByFormat($output, $table, $input->getOption('format'));
-        } else {
-            $output->writeln('<comment>No customers found</comment>');
-        }
-        return 0;
+        return $this->data;
     }
 }
