3e2403ebbba27d9e9a5c4c330f71e61d4f960b65
Sven Reichel <github-sr@hotmail.com>
2024-06-10 11:42:18 +0200

    [skip ci] Updated script:repo:list command

--- src/N98/Magento/Command/Script/Repository/ListCommand.php
+++ src/N98/Magento/Command/Script/Repository/ListCommand.php
@@ -1,31 +1,37 @@
 <?php
 
+declare(strict_types=1);
+
 namespace N98\Magento\Command\Script\Repository;
 
 use Description;
 use Location;
-use N98\Util\Console\Helper\Table\Renderer\RendererFactory;
-use N98\Util\Console\Helper\TableHelper;
+use N98\Magento\Command\AbstractMagentoCommandFormatInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class ListCommand extends AbstractRepositoryCommand
+class ListCommand extends AbstractRepositoryCommand implements AbstractMagentoCommandFormatInterface
 {
-    protected function configure()
-    {
-        $this
-            ->setName('script:repo:list')
-            ->setDescription('Lists all scripts in repository')
-            ->addOption(
-                'format',
-                null,
-                InputOption::VALUE_OPTIONAL,
-                'Output Format. One of [' . implode(',', RendererFactory::getFormats()) . ']'
-            )
-        ;
+    protected const NO_DATA_MESSAGE = 'No script file found';
 
-        $help = <<<HELP
+    /**
+     * @var string
+     * @deprecated with symfony 6.1
+     * @see AsCommand
+     */
+    protected static $defaultName = 'script:repo:list';
+
+    /**
+     * @var string
+     * @deprecated with symfony 6.1
+     * @see AsCommand
+     */
+    protected static $defaultDescription = 'Lists all scripts in repository.';
+
+    public function getHelp()
+    {
+        return <<<HELP
 You can organize your scripts in a repository.
 Simply place a script in folder */usr/local/share/n98-magerun/scripts* or in your home dir
 in folder *<HOME>/.n98-magerun/scripts*.
@@ -37,36 +43,43 @@ The first line of the script can contain a comment (line prefixed with #) which 
 
    $ n98-magerun.phar script:repo:list
 HELP;
-        $this->setHelp($help);
     }
 
     /**
-     * @param InputInterface  $input
-     * @param OutputInterface $output
+     * {@inheritdoc}
+     * @return array<int|string, array<string, string>>
      *
-     * @return int
+     * phpcs:disable Generic.CodeAnalysis.UnusedFunctionParameter
      */
-    protected function execute(InputInterface $input, OutputInterface $output): int
+    public function getData(InputInterface $input, OutputInterface $output): array
     {
-        $files = $this->getScripts();
-        if (count($files) > 0) {
-            $table = [];
-            foreach ($files as $file) {
-                $table[] = [substr($file['fileinfo']->getFilename(), 0, -strlen(self::MAGERUN_EXTENSION)), $file['location'], $file['description']];
+        if (is_null($this->data)) {
+            $this->data = [];
+
+            foreach ($this->getScripts() as $file) {
+                $this->data[] = [
+                    'Script'            => substr(
+                        $file['fileinfo']->getFilename(),
+                        0,
+                        -strlen(self::MAGERUN_EXTENSION)
+                    ),
+                    Location::class     => $file['location'],
+                    Description::class  => $file['description']
+                ];
             }
-        } else {
-            $table = [];
         }
 
-        if ($input->getOption('format') === null && count($table) === 0) {
-            $output->writeln('<info>no script file found</info>');
-        }
+        return $this->data;
+    }
 
-        /* @var TableHelper $tableHelper */
-        $tableHelper = $this->getHelper('table');
-        $tableHelper
-            ->setHeaders(['Script', Location::class, Description::class])
-            ->renderByFormat($output, $table, $input->getOption('format'));
-        return 0;
+    /**
+     * Skip initialisation
+     *
+     * @param bool $soft
+     * @return true
+     */
+    public function initMagento(bool $soft = false)
+    {
+        return true;
     }
 }
