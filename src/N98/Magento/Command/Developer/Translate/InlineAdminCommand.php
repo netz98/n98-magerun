e625c84c5954423eba013d5c4fa024cd99ccdf07
Sven Reichel <github-sr@hotmail.com>
2024-06-11 05:55:23 +0200

    [skip ci] Updated dev:translate:admin command

--- src/N98/Magento/Command/Developer/Translate/InlineAdminCommand.php
+++ src/N98/Magento/Command/Developer/Translate/InlineAdminCommand.php
@@ -1,44 +1,56 @@
 <?php
 
+declare(strict_types=1);
+
 namespace N98\Magento\Command\Developer\Translate;
 
 use Mage_Core_Model_Store;
 use N98\Magento\Command\AbstractMagentoStoreConfigCommand;
+use Symfony\Component\Console\Attribute\AsCommand;
 
+/**
+ * Toggle inline admin translation command
+ *
+ * @package N98\Magento\Command\Developer\Translate
+ */
 class InlineAdminCommand extends AbstractMagentoStoreConfigCommand
 {
     /**
      * @var string
+     * @deprecated with symfony 6.1
+     * @see AsCommand
      */
-    protected $configPath = 'dev/translate_inline/active_admin';
+    protected static $defaultName = 'dev:translate:admin';
+
+    /**
+     * @var string
+     * @deprecated with symfony 6.1
+     * @see AsCommand
+     */
+    protected static $defaultDescription = 'Toggle inline translation tool for admin';
 
     /**
      * @var string
      */
-    protected $toggleComment = 'Inline Translation (Admin)';
+    protected string $configPath = 'dev/translate_inline/active_admin';
 
     /**
      * @var string
      */
-    protected $commandName = 'dev:translate:admin';
+    protected string $toggleComment = 'Inline Translation (Admin)';
 
     /**
      * @var string
      */
-    protected $commandDescription = 'Toggle inline translation tool for admin';
-
-    /**
-     * @var string
-     */
-    protected $scope = self::SCOPE_GLOBAL;
+    protected string $scope = self::SCOPE_GLOBAL;
 
     /**
      * If required, handle the output and possible change of the developer IP restrictions
      *
-     * @param \Mage_Core_Model_Store $store
+     * @param Mage_Core_Model_Store $store
      * @param bool $disabled
      */
-    protected function _afterSave(Mage_Core_Model_Store $store, $disabled)
+    protected function _afterSave(Mage_Core_Model_Store $store, bool $disabled)
     {
         $this->detectAskAndSetDeveloperIp($store, $disabled);
     }
