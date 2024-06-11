807148fdc935154537b20c6b5ffca701f603a1fa
Sven Reichel <github-sr@hotmail.com>
2024-06-11 05:54:25 +0200

    [skip ci] Updated dev:translate:shop command

--- src/N98/Magento/Command/Developer/Translate/InlineShopCommand.php
+++ src/N98/Magento/Command/Developer/Translate/InlineShopCommand.php
@@ -1,39 +1,51 @@
 <?php
 
+declare(strict_types=1);
+
 namespace N98\Magento\Command\Developer\Translate;
 
 use Mage_Core_Model_Store;
 use N98\Magento\Command\AbstractMagentoStoreConfigCommand;
+use Symfony\Component\Console\Attribute\AsCommand;
 
+/**
+ * Toggle inline translation command
+ *
+ * @package N98\Magento\Command\Developer\Translate
+ */
 class InlineShopCommand extends AbstractMagentoStoreConfigCommand
 {
     /**
      * @var string
+     * @deprecated with symfony 6.1
+     * @see AsCommand
      */
-    protected $configPath = 'dev/translate_inline/active';
+    protected static $defaultName = 'dev:translate:shop';
+
+    /**
+     * @var string
+     * @deprecated with symfony 6.1
+     * @see AsCommand
+     */
+    protected static $defaultDescription = 'Toggle inline translation tool for shop';
 
     /**
      * @var string
      */
-    protected $toggleComment = 'Inline Translation';
+    protected string $configPath = 'dev/translate_inline/active';
 
     /**
      * @var string
      */
-    protected $commandName = 'dev:translate:shop';
-
-    /**
-     * @var string
-     */
-    protected $commandDescription = 'Toggle inline translation tool for shop';
+    protected string $toggleComment = 'Inline Translation';
 
     /**
      * If required, handle the output and possible change of the developer IP restrictions
      *
-     * @param \Mage_Core_Model_Store $store
+     * @param Mage_Core_Model_Store $store
      * @param bool $disabled
      */
-    protected function _afterSave(Mage_Core_Model_Store $store, $disabled)
+    protected function _afterSave(Mage_Core_Model_Store $store, bool $disabled)
     {
         $this->detectAskAndSetDeveloperIp($store, $disabled);
     }
