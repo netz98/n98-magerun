c5943a9560045d65daf9a302c71024c3ea334d46
Sven Reichel <github-sr@hotmail.com>
2024-06-11 11:16:32 +0200

    [skip ci] Updated dev:module:enable command

--- src/N98/Magento/Command/Developer/Module/Disableenable/EnableCommand.php
+++ src/N98/Magento/Command/Developer/Module/Disableenable/EnableCommand.php
@@ -1,14 +1,29 @@
 <?php
 
+declare(strict_types=1);
+
 namespace N98\Magento\Command\Developer\Module\Disableenable;
 
+use Symfony\Component\Console\Attribute\AsCommand;
+
 /**
- * Enable Magento module(s)
+ * Enable module(s) command
+ *
+ * @package N98\Magento\Command\Developer\Module\Disableenable
  */
 class EnableCommand extends AbstractCommand
 {
     /**
      * @var string
+     * @deprecated with symfony 6.1
+     * @see AsCommand
      */
-    protected $commandName = 'enable';
+    protected static $defaultName = 'dev:module:enable';
+
+    /**
+     * @var string
+     * @deprecated with symfony 6.1
+     * @see AsCommand
+     */
+    protected static $defaultDescription = 'Enables a module or all modules in codePool.';
 }
