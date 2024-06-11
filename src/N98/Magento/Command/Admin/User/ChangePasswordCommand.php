88525d66bdebf3473f604fc02d136fedbd804042
Sven Reichel <github-sr@hotmail.com>
2024-06-11 13:38:50 +0200

    [skip ci] Updated admin:user:change-password command

--- src/N98/Magento/Command/Admin/User/ChangePasswordCommand.php
+++ src/N98/Magento/Command/Admin/User/ChangePasswordCommand.php
@@ -1,24 +1,57 @@
 <?php
 
+declare(strict_types=1);
+
 namespace N98\Magento\Command\Admin\User;
 
 use Exception;
 use RuntimeException;
-use Symfony\Component\Console\Helper\QuestionHelper;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\Question;
+use Throwable;
 
+/**
+ * Change admin password command
+ *
+ * @package N98\Magento\Command\Admin\User
+ */
 class ChangePasswordCommand extends AbstractAdminUserCommand
 {
+    public const COMMAND_ARGUMENT_PASSWORD = 'password';
+
+    public const COMMAND_ARGUMENT_USERNAME = 'username';
+
+    /**
+     * @var string
+     * @deprecated with symfony 6.1
+     * @see AsCommand
+     */
+    protected static $defaultName = 'admin:user:change-password';
+
+    /**
+     * @var string
+     * @deprecated with symfony 6.1
+     * @see AsCommand
+     */
+    protected static $defaultDescription = 'Changes the password of a adminhtml user.';
+
     protected function configure()
     {
         $this
-            ->setName('admin:user:change-password')
-            ->addArgument('username', InputArgument::OPTIONAL, 'Username')
-            ->addArgument('password', InputArgument::OPTIONAL, 'Password')
-            ->setDescription('Changes the password of a adminhtml user.')
+            ->addArgument(
+                self::COMMAND_ARGUMENT_USERNAME,
+                InputArgument::OPTIONAL,
+                'Username'
+            )
+            ->addArgument(
+                self::COMMAND_ARGUMENT_PASSWORD,
+                InputArgument::OPTIONAL,
+                'Password'
+            )
         ;
     }
 
@@ -27,19 +60,17 @@ class ChangePasswordCommand extends AbstractAdminUserCommand
      * @param OutputInterface $output
      *
      * @return int
+     * @throws Throwable
      */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $this->detectMagento($output);
-        if (!$this->initMagento()) {
-            return 0;
-        }
+        $this->initMagento();
 
-        /* @var QuestionHelper $dialog */
-        $dialog = $this->getHelper('question');
+        $dialog = $this->getQuestionHelper();
 
         // Username
-        if (($username = $input->getArgument('username')) == null) {
+        if (($username = $input->getArgument(self::COMMAND_ARGUMENT_USERNAME)) == null) {
             $username = $dialog->ask($input, $output, new Question('<question>Username:</question> '));
         }
 
@@ -47,11 +78,11 @@ class ChangePasswordCommand extends AbstractAdminUserCommand
         if ($user->getId() <= 0) {
             $output->writeln('<error>User was not found</error>');
 
-            return 0;
+            return Command::SUCCESS;
         }
 
         // Password
-        if (($password = $input->getArgument('password')) == null) {
+        if (($password = $input->getArgument(self::COMMAND_ARGUMENT_PASSWORD)) == null) {
             $question = new Question('<question>Password:</question> ');
             $question->setHidden(true);
             $question->setHiddenFallback(false);
@@ -69,6 +100,7 @@ class ChangePasswordCommand extends AbstractAdminUserCommand
         } catch (Exception $e) {
             $output->writeln('<error>' . $e->getMessage() . '</error>');
         }
-        return 0;
+
+        return Command::SUCCESS;
     }
 }
