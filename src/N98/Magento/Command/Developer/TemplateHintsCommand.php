595cd1738dd51ad9bc12f0c45d04bfa2c3ce017c
Sven Reichel <github-sr@hotmail.com>
2024-06-11 05:46:11 +0200

    [skip ci] Updated dev:template-hints command

--- src/N98/Magento/Command/Developer/TemplateHintsCommand.php
+++ src/N98/Magento/Command/Developer/TemplateHintsCommand.php
@@ -1,51 +1,63 @@
 <?php
 
+declare(strict_types=1);
+
 namespace N98\Magento\Command\Developer;
 
 use Mage_Core_Model_Store;
 use N98\Magento\Command\AbstractMagentoStoreConfigCommand;
+use Symfony\Component\Console\Attribute\AsCommand;
 
+/**
+ * Toggle template hints command
+ *
+ * @package N98\Magento\Command\Developer
+ */
 class TemplateHintsCommand extends AbstractMagentoStoreConfigCommand
 {
     /**
      * @var string
+     * @deprecated with symfony 6.1
+     * @see AsCommand
      */
-    protected $commandName = 'dev:template-hints';
+    protected static $defaultName = 'dev:template-hints';
+
+    /**
+     * @var string
+     * @deprecated with symfony 6.1
+     * @see AsCommand
+     */
+    protected static $defaultDescription = 'Toggles template hints';
 
     /**
      * @var string
      */
-    protected $commandDescription = 'Toggles template hints';
+    protected string $toggleComment = 'Template Hints';
 
     /**
      * @var string
      */
-    protected $toggleComment = 'Template Hints';
+    protected string $configPath = 'dev/debug/template_hints';
 
     /**
      * @var string
      */
-    protected $configPath = 'dev/debug/template_hints';
-
-    /**
-     * @var string
-     */
-    protected $scope = self::SCOPE_STORE_VIEW;
+    protected string $scope = self::SCOPE_STORE_VIEW;
 
     /**
      * Add admin store to interactive prompt
      *
      * @var bool
      */
-    protected $withAdminStore = true;
+    protected bool $withAdminStore = true;
 
     /**
      * If required, handle the output and possible change of the developer IP restrictions
      *
-     * @param \Mage_Core_Model_Store $store
+     * @param Mage_Core_Model_Store $store
      * @param bool $disabled
      */
-    protected function _afterSave(Mage_Core_Model_Store $store, $disabled)
+    protected function _afterSave(Mage_Core_Model_Store $store, bool $disabled)
     {
         $this->detectAskAndSetDeveloperIp($store, $disabled);
     }
