e752235cbfaec43b0a25b50d0cbc2fa823b44708
Sven Reichel <github-sr@hotmail.com>
2024-06-12 12:06:10 +0200

    [skip ci] Updated cache:dir:flush command

--- src/N98/Magento/Command/Cache/Dir/FlushCommand.php
+++ src/N98/Magento/Command/Cache/Dir/FlushCommand.php
@@ -8,31 +8,34 @@ use FilesystemIterator;
 use N98\Magento\Command\AbstractMagentoCommand;
 use N98\Util\Filesystem;
 use RuntimeException;
+use SplFileInfo;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 /**
- * Class FlushCommand
+ * Flush cache directory command
  *
- * @package N98\Magento\Command\Cache
+ * @package N98\Magento\Command\Cache\Dir
  */
 class FlushCommand extends AbstractMagentoCommand
 {
     /**
-     * @var OutputInterface
+     * @var string
+     * @deprecated with symfony 6.1
+     * @see AsCommand
      */
-    private $output;
+    protected static $defaultName = 'cache:dir:flush';
 
-    public const NAME = 'cache:dir:flush';
+    /**
+     * @var string
+     * @deprecated with symfony 6.1
+     * @see AsCommand
+     */
+    protected static $defaultDescription = 'Flush (empty) cache directory.';
 
-    protected function configure()
-    {
-        $this
-            ->setName(FlushCommand::NAME)
-            ->setDescription('Flush (empty) Magento cache directory');
-    }
-
-    public function getHelp()
+    public function getHelp(): string
     {
         return <<<HELP
 The default cache backend is the files cache in Magento. The default
@@ -40,7 +43,7 @@ directory of that default cache backend is the directory "var/cache"
 within the Magento web-root directory (should be blocked from external
 access).
 
-The cache:dir:flish Magerun command will remove all files within that
+The cache:dir:flush command will remove all files within that
 directory. This is currently the most purist form to reset default
 caching configuration in Magento.
 
@@ -53,14 +56,13 @@ HELP;
     /**
      * @param InputInterface $input
      * @param OutputInterface $output
-     *
      * @return int
+     *
+     * phpcs:disable Generic.CodeAnalysis.UnusedFunctionParameter
      */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $this->output = $output;
-        $this->detectMagento($output, true);
-
+        $this->detectMagento($output);
         $this->initMagento();
 
         $workingDirectory = getcwd();
@@ -69,38 +71,52 @@ HELP;
 
         $output->writeln(sprintf('<info>Flushing cache directory <comment>%s</comment></info>', $cacheDir));
 
-        $this->verbose(sprintf('<debug>root-dir: <comment>%s</comment>', $magentoRootFolder));
-        $this->verbose(sprintf('<debug>cwd: <comment>%s</comment>', $workingDirectory));
+        if ($output->isVerbose()) {
+            $output->writeln(sprintf('<debug>root-dir: <comment>%s</comment>', $magentoRootFolder));
+            $output->writeln(sprintf('<debug>cwd: <comment>%s</comment>', $workingDirectory));
+        }
 
-        $this->emptyDirectory($cacheDir);
+        $this->emptyDirectory($output, $cacheDir);
 
-        $output->writeln('Cache directory flushed');
-        return 0;
+        $output->writeln('<info>Cache directory flushed</info>');
+
+        return Command::SUCCESS;
     }
 
     /**
+     * @param OutputInterface $output
      * @param string $path
-     *
      * @return bool
      */
-    private function emptyDirectory($path)
+    private function emptyDirectory(OutputInterface $output, string $path)
     {
         $errors = [];
 
         $dir = new FilesystemIterator($path);
         foreach ($dir as $file => $info) {
+            /** @var SplFileInfo $info */
             if ($info->isDir()) {
-                $this->verbose(
-                    '<debug>Filesystem::recursiveRemoveDirectory() <comment>' . $file . '</comment></debug>'
-                );
+                if ($output->isVerbose()) {
+                    $output->writeln(
+                        sprintf(
+                            '<debug>Filesystem::recursiveRemoveDirectory() <comment>%s</comment></debug>',
+                            $file
+                        )
+                    );
+                }
+
                 if (!isset($fs)) {
                     $fs = new Filesystem();
                 }
+
                 if (!$fs->recursiveRemoveDirectory($file)) {
                     $errors[] = $file;
-                };
+                }
             } else {
-                $this->verbose('<debug>unlink() <comment>' . $file . '</comment></debug>');
+                if ($output->isVerbose()) {
+                    $output->writeln(sprintf('<debug>unlink() <comment>%s</comment></debug>', $file));
+                }
+
                 if (!unlink($file)) {
                     $errors[] = $file;
                 }
@@ -117,21 +133,5 @@ HELP;
         }
 
         throw new RuntimeException($message);
-    }
-
-    /**
-     * @param string $message
-     */
-    private function verbose($message)
-    {
-        $output = $this->output;
-
-        if (!$output) {
-            return;
-        }
-
-        if (OutputInterface::VERBOSITY_VERBOSE <= $output->getVerbosity()) {
-            $output->writeln($message);
-        }
     }
 }
