7014192b473aff9ad540a65dfcead266ad614989
Sven Reichel <github-sr@hotmail.com>
2024-06-19 14:05:41 +0200

    [skip ci] Updated dev:module:observer:list command

--- src/N98/Magento/Command/Developer/Module/Observer/ListCommand.php
+++ src/N98/Magento/Command/Developer/Module/Observer/ListCommand.php
@@ -1,99 +1,118 @@
 <?php
 
+declare(strict_types=1);
+
 namespace N98\Magento\Command\Developer\Module\Observer;
 
 use InvalidArgumentException;
 use Mage;
 use N98\Magento\Command\AbstractMagentoCommand;
-use N98\Util\Console\Helper\Table\Renderer\RendererFactory;
-use N98\Util\Console\Helper\TableHelper;
+use N98\Magento\Command\AbstractMagentoCommandFormatInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class ListCommand extends AbstractMagentoCommand
+/**
+ * List observer command
+ *
+ * @package N98\Magento\Command\Developer\Module\Observer
+ */
+class ListCommand extends AbstractMagentoCommand implements AbstractMagentoCommandFormatInterface
 {
-    protected function configure()
+    public const COMMAND_ARGUMENT_TYPE = 'type';
+
+    public const COMMAND_OPTION_SORT = 'sort';
+
+    /**
+     * @var string
+     * @deprecated with symfony 6.1
+     * @see AsCommand
+     */
+    protected static $defaultName = 'dev:module:observer:list';
+
+    /**
+     * @var string
+     * @deprecated with symfony 6.1
+     * @see AsCommand
+     */
+    protected static $defaultDescription = 'Lists all registered observers.';
+
+    protected function configure(): void
     {
         $this
-            ->setName('dev:module:observer:list')
-            ->addArgument('type', InputArgument::OPTIONAL, 'Observer type (global, admin, frontend, crontab)')
-            ->setDescription('Lists all registered observers')
-            ->addOption(
-                'format',
-                null,
-                InputOption::VALUE_OPTIONAL,
-                'Output Format. One of [' . implode(',', RendererFactory::getFormats()) . ']'
+            ->addArgument(
+                self::COMMAND_ARGUMENT_TYPE,
+                InputArgument::OPTIONAL,
+                'Observer type (global, admin, frontend, crontab)'
             )
             ->addOption(
-                'sort',
+                self::COMMAND_OPTION_SORT,
                 null,
                 InputOption::VALUE_NONE,
                 'Sort by event name ascending'
             );
+
+        parent::configure();
     }
 
     /**
-     * @param InputInterface $input
-     * @param OutputInterface $output
-     *
-     * @return int
+     * {@inheritdoc}
+     * @return array<int|string, array<string, string>>
      */
-    protected function execute(InputInterface $input, OutputInterface $output): int
+    public function getData(InputInterface $input, OutputInterface $output): array
     {
-        $this->detectMagento($output, true);
-        if (!$this->initMagento()) {
-            return 0;
-        }
+        if (is_null($this->data)) {
+            $this->data = [];
 
-        $type = $input->getArgument('type');
+            /** @var string $type */
+            $type = $input->getArgument(self::COMMAND_ARGUMENT_TYPE);
 
-        $areas = ['global', 'adminhtml', 'frontend', 'crontab'];
+            $areas = ['global', 'adminhtml', 'frontend', 'crontab'];
 
-        if ($type === null) {
-            $type = $this->askForArrayEntry($areas, $input, $output, 'Please select an area:');
-        }
-
-        if (!in_array($type, $areas)) {
-            throw new InvalidArgumentException('Invalid type! Use one of: ' . implode(', ', $areas));
-        }
-
-        if ($input->getOption('format') === null) {
-            $this->writeSection($output, 'Observers: ' . $type);
-        }
-        $frontendEvents = Mage::getConfig()->getNode($type . '/events')->asArray();
-        if (true === $input->getOption('sort')) {
-            // sorting for Observers is a bad idea because the order in which observers will be called is important.
-            ksort($frontendEvents);
-        }
-        $table = [];
-        foreach ($frontendEvents as $eventName => $eventData) {
-            $observerList = [];
-            foreach ($eventData['observers'] as $observer) {
-                $observerList[] = $this->getObserver($observer, $type);
+            if ($type === null) {
+                /** @var string $type */
+                $type = $this->askForArrayEntry($areas, $input, $output, 'Please select an area:');
             }
-            $table[] = [$eventName, implode("\n", $observerList)];
+
+            if (!in_array($type, $areas)) {
+                throw new InvalidArgumentException('Invalid type! Use one of: ' . implode(', ', $areas));
+            }
+
+            if ($input->getOption(parent::COMMAND_OPTION_FORMAT) === null) {
+                $this->writeSection($output, 'Observers: ' . $type);
+            }
+
+            $frontendEvents = $this->_getMageConfigNode($type . '/events')->asArray();
+            if (true === $input->getOption(self::COMMAND_OPTION_SORT)) {
+                // sorting for Observers is a bad idea because the order in which observers will be called is important.
+                ksort($frontendEvents);
+            }
+
+            foreach ($frontendEvents as $eventName => $eventData) {
+                $observerList = [];
+                foreach ($eventData['observers'] as $observer) {
+                    $observerList[] = $this->getObserver($observer, $type);
+                }
+                $this->data[] = [
+                    'Event' => $eventName,
+                    'Observers' => implode("\n", $observerList)
+                ];
+            }
         }
 
-        /* @var TableHelper $tableHelper */
-        $tableHelper = $this->getHelper('table');
-        $tableHelper
-            ->setHeaders(['Event', 'Observers'])
-            ->setRows($table)
-            ->renderByFormat($output, $table, $input->getOption('format'));
-        return 0;
+        return $this->data;
     }
 
     /**
-     * get observer string (list entry)
+     * Get observer string (list entry)
      *
-     * @param array  $observer
+     * @param array<string, string> $observer
      * @param string $area
-     *
      * @return string
      */
-    protected function getObserver(array $observer, $area)
+    protected function getObserver(array $observer, string $area): string
     {
         $type = $this->getObserverType($observer, $area);
 
@@ -106,18 +125,15 @@ class ListCommand extends AbstractMagentoCommand
 
         $method = isset($observer['method']) ? '::' . $observer['method'] : '';
 
-        $observer = $type . $class . $method;
-
-        return $observer;
+        return $type . $class . $method;
     }
 
     /**
-     * @param array  $observer
+     * @param array<string, string> $observer
      * @param string $area
-     *
      * @return string
      */
-    private function getObserverType(array $observer, $area)
+    private function getObserverType(array $observer, string $area): string
     {
         // singleton is the default type Mage_Core_Model_App::dispatchEvent
         $type = 'singleton';
@@ -129,8 +145,7 @@ class ListCommand extends AbstractMagentoCommand
         if (isset($observer['type'])) {
             $type = $observer['type'];
         }
-        $type = str_pad($type, 11, ' ', STR_PAD_RIGHT);
 
-        return $type;
+        return str_pad($type, 11);
     }
 }
