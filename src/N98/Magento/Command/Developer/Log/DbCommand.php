c8f2925e02aa42679704a6ea320707582640bd59
Sven Reichel <github-sr@hotmail.com>
2024-06-19 18:30:17 +0200

    [skip ci] Updated dev:log:db command

--- src/N98/Magento/Command/Developer/Log/DbCommand.php
+++ src/N98/Magento/Command/Developer/Log/DbCommand.php
@@ -1,27 +1,63 @@
 <?php
 
+declare(strict_types=1);
+
 namespace N98\Magento\Command\Developer\Log;
 
 use RuntimeException;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
+/**
+ * Toggle database log command
+ *
+ * @package N98\Magento\Command\Developer\Log
+ */
 class DbCommand extends AbstractLogCommand
 {
-    protected function configure()
+    public const COMMAND_OPTION_OFF = 'off';
+
+    public const COMMAND_OPTION_ON = 'on';
+
+    /**
+     * @var string
+     * @deprecated with symfony 6.1
+     * @see AsCommand
+     */
+    protected static $defaultName = 'dev:log:db';
+
+    /**
+     * @var string
+     * @deprecated with symfony 6.1
+     * @see AsCommand
+     */
+    protected static $defaultDescription = 'Turn on/off database query logging';
+
+    protected function configure(): void
     {
         $this
-            ->setName('dev:log:db')
-            ->addOption('on', null, InputOption::VALUE_NONE, 'Force logging')
-            ->addOption('off', null, InputOption::VALUE_NONE, 'Disable logging')
-            ->setDescription('Turn on/off database query logging');
+            ->addOption(
+                self::COMMAND_OPTION_ON,
+                null,
+                InputOption::VALUE_NONE,
+                'Force logging'
+            )
+            ->addOption(
+                self::COMMAND_OPTION_OFF,
+                null,
+                InputOption::VALUE_NONE,
+                'Disable logging'
+            )
+        ;
     }
 
     /**
      * @return string
      */
-    protected function _getVarienAdapterPhpFile()
+    protected function _getVarienAdapterPhpFile(): string
     {
         return $this->_magentoRootFolder . '/lib/Varien/Db/Adapter/Pdo/Mysql.php';
     }
@@ -29,7 +65,6 @@ class DbCommand extends AbstractLogCommand
     /**
      * @param InputInterface $input
      * @param OutputInterface $output
-     *
      * @return int
      */
     protected function execute(InputInterface $input, OutputInterface $output): int
@@ -43,29 +78,32 @@ class DbCommand extends AbstractLogCommand
         $this->_replaceVariable($input, $output, '$_logAllQueries');
 
         $output->writeln("<info>Done. You can tail <comment>" . $this->_getDebugLogFilename() . "</comment></info>");
-        return 0;
+
+        return Command::SUCCESS;
     }
 
     /**
      * @return string
      * @todo I believe 1.5 and under put this in a different filename.
      */
-    protected function _getDebugLogFilename()
+    protected function _getDebugLogFilename(): string
     {
         return 'var/debug/pdo_mysql.log';
     }
 
     /**
-     * @param InputInterface  $input
+     * @param InputInterface $input
      * @param OutputInterface $output
-     * @param string          $variable
-     *
+     * @param string $variable
      * @return void
      */
-    protected function _replaceVariable($input, $output, $variable)
+    protected function _replaceVariable(InputInterface $input, OutputInterface $output, string $variable): void
     {
         $varienAdapterPhpFile = $this->_getVarienAdapterPhpFile();
         $contents = file_get_contents($varienAdapterPhpFile);
+        if (!$contents) {
+            $contents = '';
+        }
 
         $debugLinePattern = "/protected\\s" . '\\' . $variable . "\\s*?=\\s(false|true)/m";
         preg_match($debugLinePattern, $contents, $matches);
@@ -74,9 +112,9 @@ class DbCommand extends AbstractLogCommand
         }
 
         $currentValue = $matches[1];
-        if ($input->getOption('off')) {
+        if ($input->getOption(self::COMMAND_OPTION_OFF)) {
             $newValue = 'false';
-        } elseif ($input->getOption('on')) {
+        } elseif ($input->getOption(self::COMMAND_OPTION_ON)) {
             $newValue = 'true';
         } else {
             $newValue = ($currentValue == 'false') ? 'true' : 'false';
