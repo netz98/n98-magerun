fc53547a438a62d4e65bcd8890d29f713f84aeb4
Sven Reichel <github-sr@hotmail.com>
2024-06-19 15:06:56 +0200

    [skip ci] Updated dev:class:lookup command

--- src/N98/Magento/Command/Developer/ClassLookupCommand.php
+++ src/N98/Magento/Command/Developer/ClassLookupCommand.php
@@ -1,58 +1,84 @@
 <?php
 
+declare(strict_types=1);
+
 namespace N98\Magento\Command\Developer;
 
-use Mage;
 use N98\Magento\Command\AbstractMagentoCommand;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
+/**
+ * Class lookup command
+ *
+ * @package N98\Magento\Command\Developer
+ */
 class ClassLookupCommand extends AbstractMagentoCommand
 {
-    protected function configure()
-    {
-        $this
-            ->setName('dev:class:lookup')
-            ->setDescription('Resolves a grouped class name')
-            ->addArgument('type', InputArgument::REQUIRED, 'The type of the class (helper|block|model)')
-            ->addArgument('name', InputArgument::REQUIRED, 'The grouped class name')
-        ;
-    }
+    public const COMMAND_ARGUMENT_TYPE = 'type';
+
+    public const COMMAND_ARGUMENT_NAME = 'name';
+
 
     /**
-     * @return \Mage_Core_Model_Config
+     * @var string
+     * @deprecated with symfony 6.1
+     * @see AsCommand
      */
-    protected function _getConfig()
+    protected static $defaultName = 'dev:class:lookup';
+
+    /**
+     * @var string
+     * @deprecated with symfony 6.1
+     * @see AsCommand
+     */
+    protected static $defaultDescription = 'Resolves a grouped class name.';
+
+    protected function configure(): void
     {
-        return Mage::getConfig();
+        $this
+            ->addArgument(
+                self::COMMAND_ARGUMENT_TYPE,
+                InputArgument::REQUIRED,
+                'The type of the class (helper|block|model)'
+            )
+            ->addArgument(
+                self::COMMAND_ARGUMENT_NAME,
+                InputArgument::REQUIRED,
+                'The grouped class name'
+            )
+        ;
     }
 
     /**
      * @param InputInterface $input
      * @param OutputInterface $output
-     *
      * @return int
      */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $this->detectMagento($output, true);
-        if (!$this->initMagento()) {
-            return 0;
-        }
+        $this->detectMagento($output);
+        $this->initMagento();
 
-        $resolved = $this->_getConfig()->getGroupedClassName(
-            $input->getArgument('type'),
-            $input->getArgument('name')
-        );
+        /** @var string $type */
+        $type = $input->getArgument(self::COMMAND_ARGUMENT_TYPE);
+        /** @var string $name */
+        $name = $input->getArgument(self::COMMAND_ARGUMENT_NAME);
+
+        $resolved = $this->_getMageConfig()->getGroupedClassName($type, $name);
+
         $output->writeln(
-            ucfirst($input->getArgument('type')) . ' <comment>' . $input->getArgument('name') . "</comment> " .
+            ucfirst($type) . ' <comment>' . $name . "</comment> " .
             "resolves to <comment>" . $resolved . '</comment>'
         );
 
         if (!class_exists('\\' . $resolved)) {
             $output->writeln('<info>Note:</info> Class <comment>' . $resolved . '</comment> does not exist!');
         }
-        return 0;
+
+        return Command::SUCCESS;
     }
 }
