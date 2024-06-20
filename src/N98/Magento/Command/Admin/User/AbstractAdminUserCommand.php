0f478bd3e1932c58257e00bf1dc085856dd005fe
Sven Reichel <github-sr@hotmail.com>
2024-06-20 02:03:34 +0200

    [skip ci] Updated AbstractAdminUserCommand

--- src/N98/Magento/Command/Admin/User/AbstractAdminUserCommand.php
+++ src/N98/Magento/Command/Admin/User/AbstractAdminUserCommand.php
@@ -1,32 +1,43 @@
 <?php
 
+declare(strict_types=1);
+
 namespace N98\Magento\Command\Admin\User;
 
+use Mage;
+use Mage_Admin_Model_Roles;
+use Mage_Admin_Model_Rules;
+use Mage_Admin_Model_User;
 use N98\Magento\Command\AbstractMagentoCommand;
 
+/**
+ * Class AbstractAdminUserCommand
+ *
+ * @package N98\Magento\Command\Admin\User
+ */
 abstract class AbstractAdminUserCommand extends AbstractMagentoCommand
 {
     /**
-     * @return \Mage_Core_Model_Abstract|\Mage_Admin_Model_User
+     * @return Mage_Admin_Model_User
      */
-    protected function getUserModel()
+    protected function getUserModel(): Mage_Admin_Model_User
     {
-        return $this->_getModel('admin/user', 'Mage_User_Model_User');
+        return Mage::getModel('admin/user');
     }
 
     /**
-     * @return \Mage_Core_Model_Abstract
+     * @return Mage_Admin_Model_Roles
      */
-    protected function getRoleModel()
+    protected function getRoleModel(): Mage_Admin_Model_Roles
     {
-        return $this->_getModel('admin/roles', 'Mage_User_Model_Role');
+        return Mage::getModel('admin/roles');
     }
 
     /**
-     * @return \Mage_Core_Model_Abstract
+     * @return Mage_Admin_Model_Rules
      */
-    protected function getRulesModel()
+    protected function getRulesModel(): Mage_Admin_Model_Rules
     {
-        return $this->_getModel('admin/rules', 'Mage_User_Model_Rules');
+        return Mage::getModel('admin/rules');
     }
 }
