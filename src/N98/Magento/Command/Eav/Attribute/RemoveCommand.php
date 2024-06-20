62bcfd371a7b5f4f923b26429ec5d6b019d83fdb
Sven Reichel <github-sr@hotmail.com>
2024-06-21 01:51:03 +0200

    [skip ci] Updated eav:attribute:remove command

--- src/N98/Magento/Command/Eav/Attribute/RemoveCommand.php
+++ src/N98/Magento/Command/Eav/Attribute/RemoveCommand.php
@@ -1,44 +1,76 @@
 <?php
 
+declare(strict_types=1);
+
 namespace N98\Magento\Command\Eav\Attribute;
 
+use Exception;
 use InvalidArgumentException;
 use Mage;
 use Mage_Core_Exception;
 use Mage_Eav_Model_Entity_Setup;
 use N98\Magento\Command\AbstractMagentoCommand;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 /**
- * Class RemoveCommand
+ * EAV attribute remove command
+ *
  * @package N98\Magento\Command\Eav\Attribute
  * @author Aydin Hassan <aydin@hotmail.co.uk>
  */
 class RemoveCommand extends AbstractMagentoCommand
 {
-    protected function configure()
+    public const COMMAND_ARGUMENT_ENTITY = 'entityType';
+
+    public const COMMAND_ARGUMENT_ATTRIBUTE = 'attributeCode';
+
+    /**
+     * @var string
+     * @deprecated with symfony 6.1
+     * @see AsCommand
+     */
+    protected static $defaultName = 'eav:attribute:remove';
+
+    /**
+     * @var string
+     * @deprecated with symfony 6.1
+     * @see AsCommand
+     */
+    protected static $defaultDescription = 'Removes attribute for a given attribute code.';
+
+    protected function configure(): void
     {
         $this
-            ->setName('eav:attribute:remove')
-            ->addArgument('entityType', InputArgument::REQUIRED, 'Entity Type Code like catalog_product')
-            ->addArgument('attributeCode', InputArgument::REQUIRED | InputArgument::IS_ARRAY, 'Attribute Code')
-            ->setDescription('Removes attribute for a given attribute code');
+            ->addArgument(
+                self::COMMAND_ARGUMENT_ENTITY,
+                InputArgument::REQUIRED,
+                'Entity Type Code like catalog_product'
+            )
+            ->addArgument(
+                self::COMMAND_ARGUMENT_ATTRIBUTE,
+                InputArgument::REQUIRED | InputArgument::IS_ARRAY,
+                'Attribute Code'
+            )
+        ;
     }
 
     /**
-     * @param InputInterface  $input
+     * @param InputInterface $input
      * @param OutputInterface $output
-     *
      * @return int
+     * @throws Exception
      */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $this->detectMagento($output, true);
+        $this->detectMagento($output);
         $this->initMagento();
 
-        $entityType = $input->getArgument('entityType');
+        /** @var string $entityType */
+        $entityType = $input->getArgument(self::COMMAND_ARGUMENT_ENTITY);
 
         try {
             $attributes = Mage::getModel('eav/config')->getEntityAttributeCodes($entityType);
@@ -47,7 +79,10 @@ class RemoveCommand extends AbstractMagentoCommand
         }
 
         $setup = new Mage_Eav_Model_Entity_Setup('core_setup');
-        foreach ($input->getArgument('attributeCode') as $attributeCode) {
+
+        /** @var array<int, string> $attributeCodes */
+        $attributeCodes = $input->getArgument(self::COMMAND_ARGUMENT_ATTRIBUTE);
+        foreach ($attributeCodes as $attributeCode) {
             if (!in_array($attributeCode, $attributes)) {
                 $message = sprintf(
                     'Attribute: "%s" does not exist for entity type: "%s"',
@@ -67,6 +102,7 @@ class RemoveCommand extends AbstractMagentoCommand
                 );
             }
         }
-        return 0;
+
+        return Command::SUCCESS;
     }
 }
