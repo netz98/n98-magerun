f985edbffa70b367d3fcefe7e890ab036dde306a
Sven Reichel <github-sr@hotmail.com>
2024-06-21 10:46:49 +0200

    [skip ci] Updated open-browser command

--- src/N98/Magento/Command/OpenBrowserCommand.php
+++ src/N98/Magento/Command/OpenBrowserCommand.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace N98\Magento\Command;
 
 use Mage_Core_Exception;
@@ -8,18 +10,42 @@ use Mage_Core_Model_Store;
 use N98\Util\Exec;
 use N98\Util\OperatingSystem;
 use RuntimeException;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
+/**
+ * Open browser command
+ *
+ * @package N98\Magento\Command
+ */
 class OpenBrowserCommand extends AbstractCommand
 {
+    public const COMMAND_ARGUMENT_STORE = 'store';
+    /**
+     * @var string
+     * @deprecated with symfony 6.1
+     * @see AsCommand
+     */
+    protected static $defaultName = 'open-browser';
+
+    /**
+     * @var string
+     * @deprecated with symfony 6.1
+     * @see AsCommand
+     */
+    protected static $defaultDescription = 'Open current project in browser <comment>(experimental)</comment>.';
+
     protected function configure(): void
     {
         $this
-            ->setName('open-browser')
-            ->addArgument('store', InputArgument::OPTIONAL, 'Store code or ID')
-            ->setDescription('Open current project in browser <comment>(experimental)</comment>')
+            ->addArgument(
+                self::COMMAND_ARGUMENT_STORE,
+                InputArgument::OPTIONAL,
+                'Store code or ID'
+            )
         ;
     }
 
@@ -44,18 +70,20 @@ class OpenBrowserCommand extends AbstractCommand
 
         $parameterHelper = $this->getParameterHelper();
 
-        $store = $parameterHelper->askStore($input, $output, 'store', true);
+        /** @var Mage_Core_Model_Store $store */
+        $store = $parameterHelper->askStore($input, $output, self::COMMAND_ARGUMENT_STORE, true);
         if ($store->getId() == Mage_Core_Model_App::ADMIN_STORE_ID) {
             $adminFrontName = (string) $this->_getMageConfig()->getNode('admin/routers/adminhtml/args/frontName');
             $url = rtrim($store->getBaseUrl(Mage_Core_Model_Store::URL_TYPE_WEB), '/') . '/' . $adminFrontName;
         } else {
             $url = $store->getBaseUrl() . '?___store=' . $store->getCode();
         }
-        $output->writeln('Opening URL <comment>' . $url . '</comment> in browser');
+        $output->writeln(sprintf('Opening URL <comment>%s</comment> in browser', $url));
 
         $opener = $this->resolveOpenerCommand($output);
         Exec::run(escapeshellcmd($opener . ' ' . $url));
-        return 0;
+
+        return Command::SUCCESS;
     }
 
     /**
