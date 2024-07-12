7cd0f0bffd9046f1bbafd7c0e304aa0b91b9e975
Sven Reichel <github-sr@hotmail.com>
2024-07-12 23:36:01 +0200

    Flush EAV cache after adding attributes during tests

--- tests/N98/Magento/Command/Eav/Attribute/RemoveCommandTest.php
+++ tests/N98/Magento/Command/Eav/Attribute/RemoveCommandTest.php
@@ -173,6 +173,9 @@ class RemoveCommandTest extends TestCase
     {
         $setup = Mage::getModel('eav/entity_setup', 'core_setup');
         $setup->addAttribute($entityType, $attributeCode, $data);
+
+        // required with EAV attribute caching added in OpenMage 20.1.0
+        Mage::app()->getCacheInstance()->cleanType('eav');
     }
 
     /**
