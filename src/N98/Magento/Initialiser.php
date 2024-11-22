03bcfa0683b173009dbd67bb5e859a27d8290ed1
Sven Reichel <github-sr@hotmail.com>
2024-11-22 18:08:57 +0100

    revert change

--- src/N98/Magento/Initialiser.php
+++ src/N98/Magento/Initialiser.php
@@ -61,11 +61,15 @@ class Initialiser
      */
     public function requireMage()
     {
-        if (!class_exists(self::CLASS_MAGE, false)) {
-            throw new RuntimeException(sprintf('Failed to load definition of "%s" class', self::CLASS_MAGE));
+        if (class_exists(self::CLASS_MAGE, false)) {
+            return;
         }
 
         $this->requireOnce();
+
+        if (!class_exists(self::CLASS_MAGE, false)) {
+            throw new RuntimeException(sprintf('Failed to load definition of "%s" class', self::CLASS_MAGE));
+        }
     }
 
     /**
